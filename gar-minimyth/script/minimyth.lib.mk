7167398115eba84731763761ec0670e66824098f
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-13 15:30:14 +0000

    perforce changeset 533

--- gar-minimyth/script/minimyth.lib.mk
+++ gar-minimyth/script/minimyth.lib.mk
@@ -17,9 +17,12 @@ gar-patch-%:
 		| sed 's%@GAR_includedir@%$(includedir)%g' \
 		| sed 's%@GAR_libdir@%$(libdir)%g' \
 		| sed 's%@GAR_sysconfdir@%$(sysconfdir)%g' \
+		| sed 's%@GAR_x11bprefix@%$(x11prefix)%g' \
 		| sed 's%@GAR_x11bindir@%$(x11bindir)%g' \
 		| sed 's%@GAR_x11includedir@%$(x11includedir)%g' \
 		| sed 's%@GAR_x11libdir@%$(x11libdir)%g' \
+		| sed 's%@GAR_x11mandir@%$(x11mandir)%g' \
+		| sed 's%@GAR_x11sysconfdir@%$(x11sysconfdir)%g' \
 		| sed 's%@GAR_CFLAGS@%$(CFLAGS)%g' \
 		| sed 's%@GAR_CXXFLAGS@%$(CFLAGS)%g' \
 		| $(GARPATCH)
