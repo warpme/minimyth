e1a0e62bb1ebb20eca7505380435198ff034c634
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-13 06:14:11 +0000

    perforce changeset 191

--- gar-minimyth/script/meta/minimyth/dirs/etc/rc.d/mount.mk
+++ gar-minimyth/script/meta/minimyth/dirs/etc/rc.d/mount.mk
@@ -11,12 +11,12 @@ mount: /mnt/gallery /mnt/music /mnt/video
 
 /mnt/gallery: mm_var_get mm_url_mount mm_mythdb_setting_update /mnt
 	$(if $(MM_MYTHGALLERY_URL),mm_url_mount "$(MM_MYTHGALLERY_URL)" "/mnt/gallery")
-	$(if $(MM_MYTHGALLERY_URL),mm_mythdb_setting_update "GalleryDir" "/mnt/gallery/")
+	$(if $(MM_MYTHGALLERY_URL),mm_mythdb_setting_update "GalleryDir" "/mnt/gallery")
 
 /mnt/music: mm_var_get mm_url_mount mm_mythdb_setting_update /mnt
 	$(if $(MM_MYTHMUSIC_URL),mm_url_mount "$(MM_MYTHMUSIC_URL)" "/mnt/music")
-	$(if $(MM_MYTHMUSIC_URL),mm_mythdb_setting_update "MusicLocation" "/mnt/music/")
+	$(if $(MM_MYTHMUSIC_URL),mm_mythdb_setting_update "MusicLocation" "/mnt/music")
 
 /mnt/video: mm_var_get mm_url_mount mm_mythdb_setting_update /mnt
-	$(if $(MM_MYTHVIDEO_URL),mm_url_mount "$(MM_MYTHMUSIC_URL)" "/mnt/video")
-	$(if $(MM_MYTHVIDEO_URL),mm_mythdb_setting_update "VideoStartupDir" "/mnt/video/")
+	$(if $(MM_MYTHVIDEO_URL),mm_url_mount "$(MM_MYTHVIDEO_URL)" "/mnt/video")
+	$(if $(MM_MYTHVIDEO_URL),mm_mythdb_setting_update "VideoStartupDir" "/mnt/video")
