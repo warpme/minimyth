77c53cdd222d1ac7a9659bb75e289bdea6f107f2
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-13 17:30:32 +0000

    perforce changeset 628

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -4,57 +4,60 @@
 #-------------------------------------------------------------------------------
 
 # The version of MiniMyth.
-mm_VERSION        ?= 0.18.1.11
+mm_VERSION           ?= 0.18.1.11
 
 # The version of Myth to use.
 # Valid values are 'stable', 'knoppmyth' and 'svn'.
-mm_MYTH_VERSION   ?= stable
+mm_MYTH_VERSION      ?= stable
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
 #-------------------------------------------------------------------------------
 # Lists the chipset families supported.
 # Valid values for mm_CHIPSETS are one or more of 'nvidia' and 'via'.
-mm_CHIPSETS       ?= via
+mm_CHIPSETS          ?= via
 # Indicates the microprocessor architecture.
 # Valid values for mm_GARCH are 'c3', 'c3-2', 'pentium-mmx' and 'athlon64'.
-mm_GARCH          ?= pentium-mmx
+mm_GARCH             ?= pentium-mmx
 # Indicates whether or not to install the CRAMFS (ramdisk) root file system image.
 # Valid values for mm_INSTALL_CRAMFS are 'yes' and 'no'.
-mm_INSTALL_CRAMFS ?= yes
+mm_INSTALL_CRAMFS    ?= yes
 # Indicates whether or not to install the NFS root file system image.
 # Valid values for mm_INSTALL_NFS are 'yes' and 'no'.
-mm_INSTALL_NFS    ?= no
+mm_INSTALL_NFS       ?= no
 # Indicates the directory where the GAR MiniMyth build system is installed.
-mm_HOME           ?= $(P4ROOT)/gar-minimyth
+mm_HOME              ?= $(P4ROOT)/gar-minimyth
 # Indicates the pxeboot TFTP directory.
 # The kernel and the CRAMFS image are installed in this directory.
-mm_TFTP_ROOT      ?= /var/tftpboot/minimyth
+mm_TFTP_ROOT         ?= /var/tftpboot/minimyth
 # Indicates the directory in which the directory containing the NFS root file
 # system image will be installed. The name of the directory containing the NFS
 # root files sytem image is given by mm_NFSNAME.
-mm_NFS_ROOT       ?= /home/public
+mm_NFS_ROOT          ?= /home/public
 
 #-------------------------------------------------------------------------------
 # Variables that you are not likely to override.
 #-------------------------------------------------------------------------------
-mm_GARHOST        ?= $(strip \
-                         $(if $(filter athlon64   ,$(mm_GARCH)),x86_64) \
-                         $(if $(filter c3         ,$(mm_GARCH)),i586  ) \
-                         $(if $(filter c3-2       ,$(mm_GARCH)),i586  ) \
-                         $(if $(filter pentium-mmx,$(mm_GARCH)),i586  ) \
-                      )-minimyth-linux-gnu
-mm_NAME           ?= minimyth-$(mm_VERSION)
-mm_NAME_PRETTY    ?= MiniMyth $(mm_VERSION)
-mm_BASEDIR        ?= $(mm_HOME)/images/mm
-mm_TFTPDIR        ?= $(mm_TFTP_ROOT)
-mm_NFSDIR         ?= $(mm_NFS_ROOT)
-mm_KERNELNAME     ?= kernel-$(mm_NAME)
-mm_CRAMFSNAME     ?= rootfs-$(mm_NAME)
-mm_EXTRASNAME     ?= extras-$(mm_NAME).cmg
-mm_NFSNAME        ?= $(mm_NAME)
-mm_DESTDIR        ?= $(mm_BASEDIR)/rootfs-$(mm_NAME).d
-mm_EXTRASDIR      ?= $(mm_BASEDIR)/extras-$(mm_NAME).d
+mm_GARHOST           ?= $(strip \
+                            $(if $(filter athlon64   ,$(mm_GARCH)),x86_64) \
+                            $(if $(filter c3         ,$(mm_GARCH)),i586  ) \
+                            $(if $(filter c3-2       ,$(mm_GARCH)),i586  ) \
+                            $(if $(filter pentium-mmx,$(mm_GARCH)),i586  ) \
+                         )-minimyth-linux-gnu
+mm_NAME              ?= minimyth-$(mm_VERSION)
+mm_NAME_PRETTY       ?= MiniMyth $(mm_VERSION)
+mm_BASEDIR           ?= $(mm_HOME)/images/mm
+mm_TFTPDIR           ?= $(mm_TFTP_ROOT)
+mm_NFSDIR            ?= $(mm_NFS_ROOT)
+mm_KERNELNAME        ?= kernel-$(mm_NAME)
+mm_CRAMFSNAME        ?= rootfs-$(mm_NAME)
+mm_EXTRASNAME        ?= extras-$(mm_NAME).cmg
+mm_NFSNAME           ?= $(mm_NAME)
+mm_DESTDIR           ?= $(mm_BASEDIR)/rootfs-$(mm_NAME).d
+mm_EXTRASDIR         ?= $(mm_BASEDIR)/extras-$(mm_NAME).d
+# The version of Myth checked out from SVN when mm_MYTH_VERSION=svn.
+# If this is set, the patch files for myth/myth*-svn may fail.
+mm_MYTH_VERSION_SVN  ?=
 
 #-------------------------------------------------------------------------------
 # Variables that you cannot override.
