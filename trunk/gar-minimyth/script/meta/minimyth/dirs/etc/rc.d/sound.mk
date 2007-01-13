b8dace97c2bb3f333ed8144979e54d1f3b641a7a
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-13 06:16:28 +0000

    perforce changeset 195

--- trunk/gar-minimyth/script/meta/minimyth/dirs/etc/rc.d/sound.mk
+++ trunk/gar-minimyth/script/meta/minimyth/dirs/etc/rc.d/sound.mk
@@ -6,7 +6,7 @@ MM_SOUND = $(shell mm_var_get MM_SOUND)
 
 SOUND = $(if $(MM_SOUND),$(MM_SOUND),"analog")
 
-sound: sound-modules sound-config sound-mythdb
+sound: sound-modules sound-config
 	touch sound
 
 sound-modules:
@@ -28,18 +28,6 @@ sound-config: mm_var_get sound-modules /root/.xine/config
 		sed -i 's%@SPEAKER_ARRANGEMENT@%Pass Through%' /root/.xine/config ; \
 	fi
 	touch sound-config
-
-sound-mythdb: mm_var_get mm_mythdb_setting_update
-	if   [ "$(SOUND)" = "analog"  ] ; then \
-		mm_mythdb_setting_update "AudioOutputDevice" "ALSA:default" ; \
-		mm_mythdb_setting_update "MythControlsVolume" "0" ; \
-		mm_mythdb_setting_update "AudioDevice" "ALSA:default" ; \
-	elif [ "$(SOUND)" = "digital" ] ; then \
-		mm_mythdb_setting_update "AudioOutputDevice" "ALSA:iec958" ; \
-		mm_mythdb_setting_update "MythControlsVolume" "0" ; \
-		mm_mythdb_setting_update "AudioDevice" "ALSA:default" ; \
-	fi
-	touch sound-mythdb
 
 /root/.xine/config: /root
 	touch /root/.xine/config
