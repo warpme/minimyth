11c4f94e9423adea51cef0403ea9a65724fec033
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-13 14:36:44 +0000

    perforce changeset 437

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -4,7 +4,7 @@
 #-------------------------------------------------------------------------------
 
 # The version of MiniMyth.
-mm_VERSION        ?= 0.18.1.5
+mm_VERSION        ?= 0.18.1.6
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
