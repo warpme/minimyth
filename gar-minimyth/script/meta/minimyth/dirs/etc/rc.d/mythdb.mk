6f8077eb48d0f8473ef62b34a359127c208899da
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-13 06:15:16 +0000

    perforce changeset 192

--- gar-minimyth/script/meta/minimyth/dirs/etc/rc.d/mythdb.mk
+++ gar-minimyth/script/meta/minimyth/dirs/etc/rc.d/mythdb.mk
@@ -2,23 +2,7 @@
 # mythdb.mk
 ################################################################################
 
-mythdb: mythdb-minimyth mythdb-user
-	touch mythdb
-
-mythdb-minimyth: mm_mythdb_setting_update
-	mm_mythdb_setting_update "CDDevice"          "/dev/cdrom"
-	mm_mythdb_setting_update "CDWriterEnabled"   "0"
-	mm_mythdb_setting_update "DVDDeviceLocation" "/dev/dvd"
-	mm_mythdb_setting_update "EnableXbox"        "0"
-	mm_mythdb_setting_update "DecodeExtraAudio"  "0"
-	mm_mythdb_setting_update "RealtimePriority"  "1"
-	mm_mythdb_setting_update "UseMPEG2Dec"       "0"
-	mm_mythdb_setting_update "UseXVMC"           "0"
-	mm_mythdb_setting_update "UseXvMcVld"        "1"
-	mm_mythdb_setting_update "VCDDeviceLocation" "/dev/cdrom"
-	touch mythdb-minimyth
-
-mythdb-user: mm_var_get mm_mythdb_setting_update mythdb-minimyth
+mythdb: mm_var_get mm_mythdb_setting_update
 	cat /etc/minimyth.d/minimyth.conf | grep '^MM_MYTHDB_SETTING_' | sed -e 's%^MM_MYTHDB_SETTING_%%' | \
 	while read setting ; do \
 		setting_label=`echo "$${setting}" | \
@@ -35,4 +19,4 @@ mythdb-user: mm_var_get mm_mythdb_setting_update mythdb-minimyth
 			sed "s%'$$%%"`      ; \
 		mm_mythdb_setting_update "$${setting_label}" "$${setting_value}" ; \
 	done
-	touch mythdb-user
+	touch mythdb
