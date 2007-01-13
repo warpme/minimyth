0caa6a013e84e640ceb2513764be9411f5d870f8
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-13 05:25:39 +0000

    perforce changeset 62

--- trunk/gar-minimyth/script/minimyth.lib.mk
+++ trunk/gar-minimyth/script/minimyth.lib.mk
@@ -1,5 +1,25 @@
+PATCHDIR = $(WORKSRC)
+
 clean-image:
 	@rm -rf $(COOKIEROOTDIR)/$(DESTIMG).d
 	@rm -rf $(WORKROOTDIR)/$(DESTIMG).d
 	@rm -rf $(SCRATCHDIR)/$(DESTIMG).d
 	@rm -rf $(WORKROOTDIR)/$(DESTIMG).d
+
+patch-%.gar: gar-patch-%.gar
+	@$(MAKECOOKIE)
+
+gar-patch-%:
+	@echo " ==> Applying patch $(DOWNLOADDIR)/$*"
+	@cat $(DOWNLOADDIR)/$* \
+		| sed 's%@DESTDIR@%$(DESTDIR)%g' \
+		| sed 's%@prefix@%$(prefix)%g' \
+		| sed 's%@bindir@%$(bindir)%g' \
+		| sed 's%@datadir@%$(datadir)%g' \
+		| sed 's%@includedir@%$(includedir)%g' \
+		| sed 's%@libdir@%$(libdir)%g' \
+		| sed 's%@x11libdir@%$(x11libdir)%g' \
+		| sed 's%@CFLAGS@%$(CFLAGS)%g' \
+		| sed 's%@CXXFLAGS@%$(CFLAGS)%g' \
+		| $(GARPATCH)
+	@$(MAKECOOKIE)
