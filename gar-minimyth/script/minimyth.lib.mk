400d58c7187f1ab9faca4a0a32d69ef3934ab2e0
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-13 15:23:22 +0000

    perforce changeset 520

--- gar-minimyth/script/minimyth.lib.mk
+++ gar-minimyth/script/minimyth.lib.mk
@@ -10,17 +10,17 @@ patch-%.gar: gar-patch-%.gar
 gar-patch-%:
 	@echo " ==> Applying patch $(DOWNLOADDIR)/$*"
 	@cat $(DOWNLOADDIR)/$* \
-		| sed 's%@DESTDIR@%$(DESTDIR)%g' \
-		| sed 's%@prefix@%$(prefix)%g' \
-		| sed 's%@bindir@%$(bindir)%g' \
-		| sed 's%@datadir@%$(datadir)%g' \
-		| sed 's%@includedir@%$(includedir)%g' \
-		| sed 's%@libdir@%$(libdir)%g' \
-		| sed 's%@sysconfdir@%$(sysconfdir)%g' \
-		| sed 's%@x11bindir@%$(x11bindir)%g' \
-		| sed 's%@x11includedir@%$(x11includedir)%g' \
-		| sed 's%@x11libdir@%$(x11libdir)%g' \
-		| sed 's%@CFLAGS@%$(CFLAGS)%g' \
-		| sed 's%@CXXFLAGS@%$(CFLAGS)%g' \
+		| sed 's%@GAR_DESTDIR@%$(DESTDIR)%g' \
+		| sed 's%@GAR_prefix@%$(prefix)%g' \
+		| sed 's%@GAR_bindir@%$(bindir)%g' \
+		| sed 's%@GAR_datadir@%$(datadir)%g' \
+		| sed 's%@GAR_includedir@%$(includedir)%g' \
+		| sed 's%@GAR_libdir@%$(libdir)%g' \
+		| sed 's%@GAR_sysconfdir@%$(sysconfdir)%g' \
+		| sed 's%@GAR_x11bindir@%$(x11bindir)%g' \
+		| sed 's%@GAR_x11includedir@%$(x11includedir)%g' \
+		| sed 's%@GAR_x11libdir@%$(x11libdir)%g' \
+		| sed 's%@GAR_CFLAGS@%$(CFLAGS)%g' \
+		| sed 's%@GAR_CXXFLAGS@%$(CFLAGS)%g' \
 		| $(GARPATCH)
 	@$(MAKECOOKIE)
