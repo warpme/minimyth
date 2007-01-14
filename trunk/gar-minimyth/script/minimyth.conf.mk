10630c19292b7d2a5a203bbed294aa313bc3d0af
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-14 02:44:01 +0000

    perforce changeset 1079

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -72,7 +72,7 @@ mm_XORG_VERSION      ?= 7.2
 mm_NVIDIA_VERSION    ?= 9631
 # Myth SVN version built. If the version changes too much then the patches may
 # no longer work.
-mm_MYTH_SVN_VERSION  ?= 12156
+mm_MYTH_SVN_VERSION  ?= 12265
 # Lists additional packages to build when minimyth is built.
 mm_USER_PACKAGES     ?=
 # Lists additional binaries to include in the MiniMyth image
