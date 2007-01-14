bca7f6476682a9a23d639fcd2219df3e4e603673
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-14 01:29:27 +0000

    perforce changeset 1018

--- gar-minimyth/script/xorg-7.0/xorg/package-api.mk
+++ gar-minimyth/script/xorg-7.0/xorg/package-api.mk
@@ -1,12 +1,12 @@
 XORG_VERSION      = 7.0
 XORG_MASTER_SITES = \
-	$(foreach                                                                  \
-		dir,                                                               \
-		app data doc driver exerything extras font lib proto util xserver, \
-		http://xorg.freedesktop.org/releases/X11R7.0/src/$(dir)/           \
+	$(foreach                                                           \
+		dir,                                                        \
+		app data doc driver            font lib proto util xserver, \
+	        http://xorg.freedesktop.org/releases/individual/$(dir)/     \
 	) \
-	$(foreach                                                                  \
-		dir,                                                               \
-		app data doc driver exerything extras font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/individual/$(dir)/            \
-	) \
+	$(foreach                                                           \
+		dir,                                                        \
+		app data doc driver exerything font lib proto util xserver, \
+		http://xorg.freedesktop.org/releases/X11R7.0/src/$(dir)/    \
+	)
--- gar-minimyth/script/xorg-7.2/xorg/package-api.mk
+++ gar-minimyth/script/xorg-7.2/xorg/package-api.mk
@@ -1,23 +1,23 @@
 XORG_VERSION      = 7.2-RC1
 XORG_MASTER_SITES = \
-	$(foreach                                                                  \
-		dir,                                                               \
-		app data doc driver exerything extras font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/development/X11R7.2/$(dir)/   \
+	$(foreach                                                           \
+		dir,                                                        \
+		app data doc driver exerything font lib proto util xserver, \
+	        http://xorg.freedesktop.org/releases/individual/$(dir)/     \
 	) \
-	$(foreach                                                                  \
-		dir,                                                               \
-		app data doc driver exerything extras font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/development/X11R7.1/$(dir)/   \
+	$(foreach                                                           \
+		dir,                                                        \
+		app data doc driver exerything font lib proto util xserver, \
+	        http://xorg.freedesktop.org/releases/X11R7.2/src/$(dir)/    \
 	) \
-	$(foreach                                                                  \
-		dir,                                                               \
-		app data doc driver exerything extras font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/development/X11R7.0/$(dir)/   \
+	$(foreach                                                           \
+		dir,                                                        \
+		app data doc driver exerything font lib proto util xserver, \
+	        http://xorg.freedesktop.org/releases/X11R7.1/src/$(dir)/    \
 	) \
-	$(foreach                                                                  \
-		dir,                                                               \
-		app data doc driver exerything extras font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/individual/$(dir)/            \
+	$(foreach                                                           \
+		dir,                                                        \
+		app data doc driver exerything font lib proto util xserver, \
+	        http://xorg.freedesktop.org/releases/X11R7.0/src/$(dir)/    \
 	) \
 	http://xcb.freedesktop.org/dist/
