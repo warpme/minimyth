cd9f0401657d5fc1ffddad98fc7c9e6575bd0e3d
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-01-31 14:52:35 +0000

    Updated packages 'devel/gcc', 'devel/gcc-minimal-static', devel/gcc-minimal-shared' and 'devel/valgrind'.

--- gar-minimyth/script/devel/gcc/package-api.mk
+++ gar-minimyth/script/devel/gcc/package-api.mk
@@ -1,11 +1,16 @@
 GCC_MAJOR_VERSION = 4
 GCC_MINOR_VERSION = 1
 GCC_TEENY_VERSION = 2
-GCC_EXTRA_VERSION = 20070122
+GCC_EXTRA_VERSION = 20070128
 
-GCC_VERSION = $(strip $(if $(GCC_EXTRA_VERSION), \
-	$(GCC_MAJOR_VERSION).$(GCC_MINOR_VERSION)-$(GCC_EXTRA_VERSION), \
-	$(GCC_MAJOR_VERSION).$(GCC_MINOR_VERSION).$(GCC_TEENY_VERSION)))
+#GCC_TYPE = release
+GCC_TYPE = prerelease
+#GCC_TYPE = snapshot
+
+GCC_VERSION = $(strip                                                                                                              \
+	$(if $(filter release   ,$(GCC_TYPE)),$(GCC_MAJOR_VERSION).$(GCC_MINOR_VERSION).$(GCC_TEENY_VERSION)                     ) \
+	$(if $(filter prerelease,$(GCC_TYPE)),$(GCC_MAJOR_VERSION).$(GCC_MINOR_VERSION).$(GCC_TEENY_VERSION)-$(GCC_EXTRA_VERSION)) \
+	$(if $(filter snapshot  ,$(GCC_TYPE)),$(GCC_MAJOR_VERSION).$(GCC_MINOR_VERSION)-$(GCC_EXTRA_VERSION)                     ) )
 
 CROSS_GCC_DIR = \
 	$(build_DESTDIR)$(build_libdir)/gcc/$(GARTARGET)
