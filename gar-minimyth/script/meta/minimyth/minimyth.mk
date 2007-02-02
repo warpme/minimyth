87d756235b092001b4a5bd93f6510641bd8e70b6
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-02-02 06:27:24 +0000

    - Made list of kernel modules in the minimyth-lib-list* files more explicit. - Removed unneeded debug output from minimyth.mk.

--- gar-minimyth/script/meta/minimyth/minimyth.mk
+++ gar-minimyth/script/meta/minimyth/minimyth.mk
@@ -476,9 +476,8 @@ mm-copy-kernel-modules:
 	@# Copy dependent kernel modules.
 	@echo 'copying dependent kernel modules'
 	@depmod -b "$(DESTDIR)$(rootdir)" "$(LINUX_FULL_VERSION)"
-	find $(mm_ROOTFSDIR)$(LINUX_MODULESDIR) -name '*.ko' | sed -e 's%^$(mm_ROOTFSDIR)%/%' -e 's%//*%/%g' | \
+	@find $(mm_ROOTFSDIR)$(LINUX_MODULESDIR) -name '*.ko' | sed -e 's%^$(mm_ROOTFSDIR)%/%' -e 's%//*%/%g' | \
 	while read module ; do \
-		echo $${module} ; \
 		module_deps=`cat $(DESTDIR)$(LINUX_MODULESDIR)/modules.dep | grep -e "^$${module}:" | sed -e 's%[^:]*: *%%'` ; \
 		for module_dep in $${module_deps} ; do \
 			if [ ! -e $(mm_ROOTFSDIR)$${module_dep} ] ; then \
