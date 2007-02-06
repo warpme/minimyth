7e6fa5320ed87d3cf976f565e98588d660c9e4a7
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-02-06 06:11:57 +0000

    - Removed unneeded MASTER_SITES from 'xorg-7.2/*'. - Fixed error in 'xorg-*/xorg/package-api.mk'.

--- gar-minimyth/script/xorg-7.0/xorg/package-api.mk
+++ gar-minimyth/script/xorg-7.0/xorg/package-api.mk
@@ -7,6 +7,6 @@ XORG_MASTER_SITES = \
 	) \
 	$(foreach                                                           \
 		dir,                                                        \
-		app data doc driver exerything font lib proto util xserver, \
+		app data doc driver everything font lib proto util xserver, \
 		http://xorg.freedesktop.org/releases/X11R7.0/src/$(dir)/    \
 	)
--- gar-minimyth/script/xorg-7.2/xorg/package-api.mk
+++ gar-minimyth/script/xorg-7.2/xorg/package-api.mk
@@ -2,22 +2,12 @@ XORG_VERSION      = 7.2-RC3
 XORG_MASTER_SITES = \
 	$(foreach                                                           \
 		dir,                                                        \
-		app data doc driver exerything font lib proto util xserver, \
+		app data doc driver everything font lib proto util xserver, \
 	        http://xorg.freedesktop.org/releases/individual/$(dir)/     \
 	) \
 	$(foreach                                                           \
 		dir,                                                        \
-		app data doc driver exerything font lib proto util xserver, \
+		app data doc driver everything font lib proto util xserver, \
 	        http://xorg.freedesktop.org/releases/X11R7.2/src/$(dir)/    \
-	) \
-	$(foreach                                                           \
-		dir,                                                        \
-		app data doc driver exerything font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/X11R7.1/src/$(dir)/    \
-	) \
-	$(foreach                                                           \
-		dir,                                                        \
-		app data doc driver exerything font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/X11R7.0/src/$(dir)/    \
 	) \
 	http://xcb.freedesktop.org/dist/
