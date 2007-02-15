4c5141dcac1217a3ba232f333562ea708bb4408c
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-02-15 15:51:14 +0000

    - Updated packages 'devel/gcc', 'devel/gcc-minimal-static' and 'devel/gcc-minimal-shared' to GCC 4.1.2.

--- gar-minimyth/script/devel/gcc/package-api.mk
+++ gar-minimyth/script/devel/gcc/package-api.mk
@@ -1,10 +1,10 @@
 GCC_MAJOR_VERSION = 4
 GCC_MINOR_VERSION = 1
 GCC_TEENY_VERSION = 2
-GCC_EXTRA_VERSION = 20070208
+GCC_EXTRA_VERSION =
 
-#GCC_TYPE = release
-GCC_TYPE = prerelease
+GCC_TYPE = release
+#GCC_TYPE = prerelease
 #GCC_TYPE = snapshot
 
 GCC_VERSION = $(strip                                                                                                              \
