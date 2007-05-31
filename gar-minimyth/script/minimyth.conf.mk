11a3d5dc74be29eb93891184ca049a391abbbdc8
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-05-31 02:51:35 +0000

    - Replaced package 'nvidia/nvidia-9631' with 'nvidia/nvidia-9639'. - Updated package 'system/lirc'.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -103,7 +103,7 @@ mm_KERNEL_CONFIG      ?=
 # Valid values are 'stable19', 'stable20', 'softpad20' and 'svn'.
 mm_MYTH_VERSION       ?= stable20
 # The version of the NVIDIA driver.
-# Valid values are '7185' (legacy), '9631' (legacy), '9746', '9755' and '100.14.06' (beta)
+# Valid values are '7185' (legacy), '9639' (legacy), '9746', '9755' and '100.14.06' (beta)
 mm_NVIDIA_VERSION     ?= 9746
 # The version of xorg to use.
 # Valid values are '7.2' and '7.3' (pre-release).
