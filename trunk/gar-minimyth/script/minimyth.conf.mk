e3eed4c8054a734beaf200efb15a7595cda49d33
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-11-03 19:20:07 +0000

    - Changed the default NVIDIA driver to 100.14.19.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -106,7 +106,7 @@ mm_KERNEL_CONFIG          ?=
 mm_MYTH_VERSION           ?= stable20
 # The version of the NVIDIA driver.
 # Valid values are '7185' (legacy), '71.86.01' (legacy), '9639' (legacy), '96.43.01' (legacy), '9746', '9755' and '100.14.19'.
-mm_NVIDIA_VERSION         ?= 9746
+mm_NVIDIA_VERSION         ?= 100.14.19
 # The version of xorg to use.
 # Valid values are '7.2' and '7.3'.
 mm_XORG_VERSION           ?= 7.2
