6a58de052838bde6ad8c3510c6decd1bc22eba4a
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-11-30 18:30:59 +0000

    Improved security implementation.   - Added MM_SECURITY_ENABLED and obsoleted MM_SECURITY_LEVEL.   - Added MM_SECURITY_USER_MINIMYTH_UID and MM_SECURITY_USER_MINIMYTH_GID     for setting the UID and GID used by the user 'minimyth', making it     easier to interoperate more securely with NFS shares.   - Added MM_SECURITY_FETCH_CREDENTIALS_CIFS for fetch a CIFS credentials     file that can be used by mount.cifs when connecting to CIFS shares.   - Disabled password access for users 'root' and 'minimyth' when security     is enabled and not debugging.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -17,7 +17,7 @@ mm_VERSION_EXTRA          ?= $(strip \
                               )
 
 # Configuration file (minimyth.conf) version.
-mm_CONF_VERSION           ?= 15
+mm_CONF_VERSION           ?= 16
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
