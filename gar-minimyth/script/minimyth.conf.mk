d8f81c4458b5c1456c86591f01d7953b4254c1c7
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-12-06 18:55:51 +0000

    - Changed NVIDIA driver version to 169.04.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -107,7 +107,7 @@ mm_MYTH_VERSION           ?= stable20
 # The version of the NVIDIA driver.
 # Valid values are '7185' (legacy), '71.86.01' (legacy), '9639' (legacy), '96.43.01' (legacy),
 # '9746', '100.14.19' and '169.04' (beta).
-mm_NVIDIA_VERSION         ?= 9746
+mm_NVIDIA_VERSION         ?= 169.04
 # The version of xorg to use.
 # Valid values are '7.2' and '7.3'.
 mm_XORG_VERSION           ?= 7.2
