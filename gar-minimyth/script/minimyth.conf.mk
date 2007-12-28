4535cc6ef1fc64eb1216940d536107e9f14d2951
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2007-12-28 18:18:18 +0000

    - Bumped configuration file version to reflect recent changes.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -17,7 +17,7 @@ mm_VERSION_EXTRA          ?= $(strip \
                               )
 
 # Configuration file (minimyth.conf) version.
-mm_CONF_VERSION           ?= 18
+mm_CONF_VERSION           ?= 19
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
