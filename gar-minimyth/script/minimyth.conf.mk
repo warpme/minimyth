a0a4dbb89e6c24795bec6cd3759102e425325281
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-01-12 22:50:39 +0000

    - Bumped MiniMyth configuration version to reflact recent variable renaming.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -17,7 +17,7 @@ mm_VERSION_EXTRA          ?= $(strip \
                               )
 
 # Configuration file (minimyth.conf) version.
-mm_CONF_VERSION           ?= 20
+mm_CONF_VERSION           ?= 21
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
