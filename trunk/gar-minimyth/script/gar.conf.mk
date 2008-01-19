332e1bb6c5de83c937c3668e85a08f175f59c768
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-01-19 02:43:30 +0000

    - add '-Wl,--as-needed' to the cross environment LDFLAGS.

--- trunk/gar-minimyth/script/gar.conf.mk
+++ trunk/gar-minimyth/script/gar.conf.mk
@@ -105,7 +105,7 @@ build_chroot_DESTDIR ?= /tmp/chroot
 # allow us to link to libraries we installed
 main_CPPFLAGS += 
 main_CFLAGS += $(mm_CFLAGS)
-main_LDFLAGS += 
+main_LDFLAGS += -Wl,--as-needed
 main_CXXFLAGS += $(main_CFLAGS)
 
 # allow us to link to libraries we installed
