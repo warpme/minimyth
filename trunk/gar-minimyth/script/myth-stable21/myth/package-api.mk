15d2337f3478ea7a94eebb2e7b51998b71873bd6
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-02-12 19:10:15 +0000

    - Added MythTV 0.20 support to the build system.   - Does not work because MythTV 0.21 tarballs have not been released.

---
+++ trunk/gar-minimyth/script/myth-stable21/myth/package-api.mk
@@ -0,0 +1,58 @@
+MYTHTV_SOURCEDIR = $(sourcedir)/mythtv
+
+MYTHTV_STABLE21_FIXES_VERSION = 15948
+
+MYTHTV_CONFIGURE_ENV = \
+	DESTDIR="$(DESTDIR)" \
+	QTDIR="$(DESTDIR)$(qtprefix)" \
+	QMAKESPEC="default" \
+	OPTFLAGS="$(CFLAGS)"
+MYTHTV_BUILD_ENV     = \
+	DESTDIR="$(DESTDIR)" \
+	QTDIR="$(DESTDIR)$(qtprefix)" \
+	QMAKESPEC="default" \
+	OPTFLAGS="$(CFLAGS)"
+MYTHTV_INSTALL_ENV   = \
+	DESTDIR="$(DESTDIR)" \
+	QTDIR="$(DESTDIR)$(qtprefix)" \
+	QMAKESPEC="default" \
+	OPTFLAGS="$(CFLAGS)" \
+	INSTALL_ROOT="$(DESTDIR)"
+
+mythtv-fixes-patch:
+	@mkdir -p $(PARTIALDIR)
+	@$(MAKE) $(DOWNLOADDIR)/$(DISTNAME).tar.bz2
+	@$(call FETCH_SVN, \
+		http://svn.mythtv.org/svn/branches/release-0-21-fixes/$(GARNAME), \
+		$(MYTHTV_STABLE21_FIXES_VERSION),  \
+		$(GARNAME)-$(MYTHTV_STABLE21_FIXES_VERSION))
+	@rm -rf $(WORKDIR)/fixes-patch
+	@mkdir -p $(WORKDIR)/fixes-patch
+	@tar -jxf $(DOWNLOADDIR)/$(DISTNAME).tar.bz2 -C $(WORKDIR)/fixes-patch
+	@tar -jxf $(PARTIALDIR)/$(GARNAME)-$(MYTHTV_STABLE21_FIXES_VERSION).tar.bz2 -C $(WORKDIR)/fixes-patch
+	@cd $(WORKDIR)/fixes-patch ; \
+		mv $(DISTNAME) $(DISTNAME)-old
+	@cd $(WORKDIR)/fixes-patch ; \
+		mv $(GARNAME)-$(MYTHTV_STABLE21_FIXES_VERSION) $(DISTNAME)-new
+	@cd $(WORKDIR)/fixes-patch ; \
+		diff -Naur -x 'mytharchive' $(DISTNAME)-old $(DISTNAME)-new > $(DISTNAME)-fixes-$(MYTHTV_STABLE21_FIXES_VERSION).patch || true
+	@if [ -w files/$(DISTNAME)-fixes-$(MYTHTV_STABLE21_FIXES_VERSION).patch ] ; then \
+		rm -f files/$(DISTNAME)-fixes-$(MYTHTV_STABLE21_FIXES_VERSION).patch ; \
+	 fi
+	@if [ -e files/$(DISTNAME)-fixes-$(MYTHTV_STABLE21_FIXES_VERSION).patch ] ; then \
+		echo "error: cannot remove existing patch file from the files directory" ; \
+		exit 1 ; \
+	 fi
+	@cp $(WORKDIR)/fixes-patch/$(DISTNAME)-fixes-$(MYTHTV_STABLE21_FIXES_VERSION).patch files/
+	@if [ ! -w checksums ] ; then \
+		echo "error: cannot write the patch file checksum to the checksum file" ; \
+		exit 1 ; \
+	 fi
+	@cat checksums \
+		| sed 's%^.*download/$(DISTNAME)-fixes-.*\.patch%%' \
+		| grep 'download' \
+		> $(WORKDIR)/fixes-patch/checksums~
+	@mv -f $(WORKDIR)/fixes-patch/checksums~ checksums
+	@md5sum files/$(DISTNAME)-fixes-$(MYTHTV_STABLE21_FIXES_VERSION).patch >> checksums
+	@sed -i 's%files/\($(DISTNAME)-fixes-$(MYTHTV_STABLE21_FIXES_VERSION).patch\)%download/\1%' checksums
+	@rm -rf $(WORKDIR)/fixes-patch
---
+++ trunk/gar-minimyth/script/myth-stable21/mythplugins/package-api.mk
@@ -0,0 +1,7 @@
+MYTHPLUGINS_VERSION = 0.21
+
+MYTHPLUGINS_CONFIGURE_ARGS = \
+	--prefix="$(prefix)" \
+	--libdir-name="lib" \
+	--disable-all \
+	--enable-opengl
