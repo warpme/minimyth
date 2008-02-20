f673001f7e73161eca2e74807a5ffe504a77a2fd
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-02-20 01:05:24 +0000

    - Factored out common perl module makefile information. - Improved perl dynloader patch.

--- trunk/gar-minimyth/script/perl/perl/package-api.mk
+++ trunk/gar-minimyth/script/perl/perl/package-api.mk
@@ -1,7 +1,8 @@
 PERL_VERSION = 5.8.8
 
-PERL_PACKAGE_DEPENDS   = perl/perl
-PERL_PACKAGE_BUILDDEPS = perl/perl
+PERL_CONFIGURE_ENV = PERL5LIB="$(PERL5LIB)"
+PERL_BUILD_ENV     = PERL5LIB="$(PERL5LIB)"
+PERL_INSTALL_ENV   = PERL5LIB="$(PERL5LIB)"
 
 # This is a hack for cross compilation, but it should does not break native compilation.
 # Ensure that everything is built using the compiler flags that were used when generating the config.sh files.
@@ -24,7 +25,7 @@ PERL_configdir = $(build_DESTDIR)$(build_libdir)/perl5/Config/$(PERL_VERSION)/$(
 # This is a hack for cross compilation, but it should does not break native compilation.
 # Ensure that packages being built have the highest chance of finding the installed packages.
 # The the *.dynloader.patch file attempts to ensure that perl will look in these directories
-# last when looking for *.so files, making less likely to break perl module execution.
+# last when looking for *.so files, making it less likely to break perl module execution.
 PERL5LIB = \
 	$(patsubst %:,%,$(subst : ,:,$(patsubst %,%:,\
 		$(PERL_configdir) \
@@ -33,16 +34,26 @@ PERL5LIB = \
 		$(PERL_libdir)/site_perl/ \
 	)))
 
-PERL_PACKAGE_DEFAULT_ARGS = \
-	DESTDIR="$(DESTDIR)"
-
-PERL_PACKAGE_DEFAULT_ENV  = \
-	PERL5LIB="$(PERL5LIB)"
-
-configure-perl-package:
-	@cd $(WORKSRC) ; $(CONFIGURE_ENV) perl Makefile.PL $(CONFIGURE_ARGS)
-	@for file in `find $(WORKSRC) -name Makefile` ; do \
+configure-%/Makefile.PL:
+	@echo " ==> Running 'perl Makefile.PL' in $*"
+	@cd $* ; $(CONFIGURE_ENV) perl Makefile.PL $(CONFIGURE_ARGS)
+	@for file in `find $* -name Makefile` ; do \
 		sed -i 's%^PERL_INC *= *%PERL_INC = $$(DESTDIR)%' $${file} ; \
 		sed -i 's%^PERL_ARCHLIB *= *%PERL_ARCHLIB = $$(DESTDIR)%' $${file} ; \
 	 done
+	@$(MAKECOOKIE)
+
+configure-%/Build.PL:
+	@echo " ==> Running 'perl Build.PL' in $*"
+	@cd $* ; $(CONFIGURE_ENV) perl Build.PL $(CONFIGURE_ARGS)
+	@$(MAKECOOKIE)
+
+build-%/Build:
+	@echo " ==> Running './Build' in $*"
+	@cd $* ; $(BUILD_ENV) ./Build $(BUILD_ARGS)
+	@$(MAKECOOKIE)
+
+install-%/Build:
+	@echo " ==> Running './Build install' in $*"
+	@cd $* ; $(INSTALL_ENV) ./Build $(INSTALL_ARGS) install
 	@$(MAKECOOKIE)
