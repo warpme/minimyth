00aa67ba685eacc0cd920890bba4cb76e9cb3415
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-02-29 23:44:05 +0000

    - Updated MythTV 0.21 packages.   - They now build.

--- gar-minimyth/script/myth-stable21/mythplugins/package-api.mk
+++ gar-minimyth/script/myth-stable21/mythplugins/package-api.mk
@@ -5,3 +5,7 @@ MYTHPLUGINS_CONFIGURE_ARGS = \
 	--libdir-name="lib" \
 	--disable-all \
 	--enable-opengl
+
+WORKSRC = $(strip $(if $(filter yes,$(MYTHTV_USE_RELEASE_TARBALL)), \
+	$(WORKDIR)/mythplugins-$(MYTHPLUGINS_VERSION), \
+	$(WORKDIR)/mythplugins-$(MYTHPLUGINS_VERSION)-$(MYTHTV_STABLE21_FIXES_VERSION)))
