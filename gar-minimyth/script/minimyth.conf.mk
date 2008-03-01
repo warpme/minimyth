731e6d6960af2f7c48a5c45091c84a7bc858947f
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-03-01 06:28:39 +0000

    - Removed support for MythAudio from MiniMyth.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -18,7 +18,7 @@ mm_VERSION_EXTRA          ?= $(strip \
                               )
 
 # Configuration file (minimyth.conf) version.
-mm_CONF_VERSION           ?= 23
+mm_CONF_VERSION           ?= 24
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
