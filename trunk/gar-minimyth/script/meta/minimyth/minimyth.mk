478e8436345776b0b14bc549eb56d19b1b6e5b43
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-03-09 03:44:51 +0000

    - Removed packages   - nvidia/nvidia-169.09

--- trunk/gar-minimyth/script/meta/minimyth/minimyth.mk
+++ trunk/gar-minimyth/script/meta/minimyth/minimyth.mk
@@ -405,7 +405,6 @@ mm-check:
 	@echo "    mm_NVIDIA_VERSION"
 	@if [ ! "$(mm_NVIDIA_VERSION)" = "71.86.04"  ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "96.43.05"  ] && \
-	    [ ! "$(mm_NVIDIA_VERSION)" = "169.09"    ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "169.12"    ] ; then \
 		echo "error: mm_NVIDIA_VERSION=\"$(mm_NVIDIA_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
