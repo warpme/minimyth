d9ac852db9e0321185d417e22fb05b44e56f7bd4
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-08-30 16:19:09 +0000

    - Updated MythTV trunk.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -38,7 +38,7 @@ mm_CHIPSETS               ?= ati intel nvidia via other
 # may be removed in the future without warning), 'vlc' (experimental and may be
 # removed in the future without warning), 'xine', 'perl', 'transcode', 'mame',
 # 'wiimote', 'backend' and 'debug'.
-mm_SOFTWARE               ?= $(if $(filter $(mm_MYTH_VERSION),0.21),mythbrowser) \
+mm_SOFTWARE               ?= mythbrowser \
                              mythgallery \
                              mythgame \
                              mythmusic \
@@ -116,7 +116,7 @@ mm_NVIDIA_VERSION         ?= 169.12
 mm_XORG_VERSION           ?= 7.3
 # Myth trunk version built. If the version changes too much then the patches may
 # no longer work.
-mm_MYTH_TRUNK_VERSION     ?= 18112
+mm_MYTH_TRUNK_VERSION     ?= 18227
 # Lists additional packages to build when minimyth is built.
 mm_USER_PACKAGES          ?=
 # Lists additional binaries to include in the MiniMyth image
