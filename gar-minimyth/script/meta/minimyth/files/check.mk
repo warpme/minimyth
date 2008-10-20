26b4de251e22ae3b025185df37f82f9af1e12008
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-10-20 17:58:02 +0000

    - Added build system support for gnash. - Added packages     lib/agg     lib/boost     lib/giflib     mediaplayers/gnash

--- gar-minimyth/script/meta/minimyth/files/check.mk
+++ gar-minimyth/script/meta/minimyth/files/check.mk
@@ -139,6 +139,7 @@ mm-all:
 		   [ ! "$${software}" = "mythvideo"      ] && \
 		   [ ! "$${software}" = "mythweather"    ] && \
 		   [ ! "$${software}" = "mythzoneminder" ] && \
+		   [ ! "$${software}" = "gnash"          ] && \
 		   [ ! "$${software}" = "mplayer"        ] && \
 		   [ ! "$${software}" = "mplayer-svn"    ] && \
 		   [ ! "$${software}" = "vlc"            ] && \
