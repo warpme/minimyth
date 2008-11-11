3452abf16dd5d6170d8a7791c711eb68e0d187c3
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-11-11 05:43:49 +0000

    - Added MM_MEDIA_GENERIC_LIST for adding media mounts that are not handled   by the MM_MEDIA_*_MOUNTPOINT + MM_MEDIA_*_URL variables.

--- trunk/gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/media.pm
+++ trunk/gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/media.pm
@@ -18,11 +18,11 @@ sub start
 
     foreach my $media (('TV', 'GALLERY', 'GAME', 'MUSIC', 'VIDEO', 'DVD_RIP'))
     {
-        my $url_name        = 'MM_MEDIA_' . $media . '_URL';
         my $mountpoint_name = 'MM_MEDIA_' . $media . '_MOUNTPOINT';
-        my $url             = $minimyth->var_get($url_name);
+        my $url_name        = 'MM_MEDIA_' . $media . '_URL';
         my $mountpoint      = $minimyth->var_get($mountpoint_name);
-        if (($url) && ($mountpoint))
+        my $url             = $minimyth->var_get($url_name);
+        if (($mountpoint) && ($url))
         {
             $minimyth->url_mount($url, $mountpoint);
             if (open(FILE, '<', '/proc/mounts'))
@@ -66,6 +66,37 @@ sub start
         }
     }
 
+    my @media_list = split(/ /, $minimyth->var_get('MM_MEDIA_GENERIC_LIST'));
+    foreach my $media (@media_list)
+    {
+        if ($media =~ /^([^=]+)=(.*)$/)
+        {
+            my $mountpoint = $1;
+            my $url        = $2;
+
+            $minimyth->url_mount($url, $mountpoint);
+            if (open(FILE, '<', '/proc/mounts'))
+            {
+                my $mounted = 0;
+                foreach (grep(/^[^ ]* $mountpoint/, (<FILE>)))
+                {
+                    $mounted = 1;
+                }
+                close(FILE);
+                if ($mounted == 0)
+                {
+                    $minimyth->message_output('err', "error: '$mountpoint' failed to mount.");
+                    return 0;
+                }
+            }
+            if (system(qq(/bin/su -c '/usr/bin/test ! -r $mountpoint' - minimyth)) == 0)
+            {
+                $minimyth->message_output('err', "error: '$mountpoint' is not readable by user 'minimyth'.");
+                return 0;
+            }
+        }
+    }
+
     $minimyth->file_replace_variable(
         '/home/minimyth/.xine/config',
         { '@MEDIA_FILES_ORIGIN_PATH@' => $minimyth->var_get('MM_MEDIA_VIDEO_MOUNTPOINT') });
@@ -80,14 +111,38 @@ sub stop
 
     $minimyth->message_output('info', "unmounting media shares ...");
 
+    my @media_list = reverse(split(/ /, $minimyth->var_get('MM_MEDIA_GENERIC_LIST')));
+    foreach my $media (@media_list)
+    {
+        if ($media =~ /^([^=]+)=(.*)$/)
+        {
+            my $mountpoint = $1;
+            my $url        = $2;
+
+            if (open(FILE, '<', '/proc/mounts'))
+            {
+                my $mounted = 0;
+                foreach (grep(/^[^ ]* $mountpoint/, (<FILE>)))
+                {
+                    $mounted = 1;
+                }
+                close(FILE);
+                if ($mounted != 0)
+                {
+                    system(qq(/bin/umount "$mountpoint"));
+                }
+            }
+        }
+    }
+
     foreach my $media (('DVD_RIP', 'VIDEO', 'MUSIC', 'GAME', 'GALLERY', 'TV'))
     {
-        my $url_name        = 'MM_MEDIA_' . $media . '_URL';
         my $mountpoint_name = 'MM_MEDIA_' . $media . '_MOUNTPOINT';
-        my $url             = $minimyth->var_get($url_name);
+        my $url_name        = 'MM_MEDIA_' . $media . '_URL';
         my $mountpoint      = $minimyth->var_get($mountpoint_name);
+        my $url             = $minimyth->var_get($url_name);
 
-        if (($url) && ($mountpoint))
+        if (($mountpoint) && ($url))
         {
             if (open(FILE, '<', '/proc/mounts'))
             {
