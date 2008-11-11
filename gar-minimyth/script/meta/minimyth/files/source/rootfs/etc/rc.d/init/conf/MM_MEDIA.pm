a816b711b78704f25d5eb0bb80aa82e3b5ed3b56
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-11-11 05:43:49 +0000

    - Added MM_MEDIA_GENERIC_LIST for adding media mounts that are not handled   by the MM_MEDIA_*_MOUNTPOINT + MM_MEDIA_*_URL variables.

--- gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/conf/MM_MEDIA.pm
+++ gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/conf/MM_MEDIA.pm
@@ -7,6 +7,24 @@ package init::conf::MM_MEDIA;
 use strict;
 use warnings;
 
+my $dir = '/|(/[[:alnum:]._-]+)+';
+my $url = '(cifs|nfs):(//(([^ :@]*)?(:([^ @]*))?\@)?([^ /]+))?[^ ?#]*(\?([^ #]*))?(\#([^ ]*))?';
+
+my $value_valid_mountpoint = qq/$dir/;
+my $value_valid_url        = qq/none|$url/;
+my $value_valid_list       = qq/none|((($dir)=($url))( ($dir)=($url))*)/;
+my $value_clean_list       = sub
+{
+    my $minimyth = shift;
+    my $name     = shift;
+
+    my $value_clean = $minimyth->var_get($name);
+    $value_clean =~ s/[ [:cntrl:]]+/ /g;
+    $value_clean =~ s/^ //g;
+    $value_clean =~ s/ $//g;
+    $minimyth->var_set($name, $value_clean);
+};
+
 my %var_list;
 
 sub var_list
@@ -16,62 +34,75 @@ sub var_list
 
 $var_list{'MM_MEDIA_TV_MOUNTPOINT'} =
 {
-    value_default  => '/mnt/tv'
+    value_default  => '/mnt/tv',
+    value_valid    => $value_valid_mountpoint
 };
 $var_list{'MM_MEDIA_TV_URL'} =
 {
     value_default  => 'none',
-    value_valid    => 'none|((cifs|nfs):(//(([^:@]*)?(:([^@]*))?\@)?([^/]+))?[^?#]*(\?([^#]*))?(\#(.*))?)',
+    value_valid    => $value_valid_url,
     value_none     => ''
 };
 $var_list{'MM_MEDIA_GALLERY_MOUNTPOINT'} =
 {
-    value_default  => '/mnt/gallery'
+    value_default  => '/mnt/gallery',
+    value_valid    => $value_valid_mountpoint
 };
 $var_list{'MM_MEDIA_GALLERY_URL'} =
 {
     value_default  => 'none',
-    value_valid    => 'none|((cifs|nfs):(//(([^:@]*)?(:([^@]*))?\@)?([^/]+))?[^?#]*(\?([^#]*))?(\#(.*))?)',
+    value_valid    => $value_valid_url,
     value_none     => ''
 };
 $var_list{'MM_MEDIA_GAME_MOUNTPOINT'} =
 {
-    value_default  => '/mnt/game'
+    value_default  => '/mnt/game',
+    value_valid    => $value_valid_mountpoint
 };
 $var_list{'MM_MEDIA_GAME_URL'} =
 {
     value_default  => 'none',
-    value_valid    => 'none|((cifs|nfs):(//(([^:@]*)?(:([^@]*))?\@)?([^/]+))?[^?#]*(\?([^#]*))?(\#(.*))?)',
+    value_valid    => $value_valid_url,
     value_none     => ''
 };
 $var_list{'MM_MEDIA_MUSIC_MOUNTPOINT'} =
 {
-    value_default  => '/mnt/music'
+    value_default  => '/mnt/music',
+    value_valid    => $value_valid_mountpoint
 };
 $var_list{'MM_MEDIA_MUSIC_URL'} =
 {
     value_default  => 'none',
-    value_valid    => 'none|((cifs|nfs):(//(([^:@]*)?(:([^@]*))?\@)?([^/]+))?[^?#]*(\?([^#]*))?(\#(.*))?)',
+    value_valid    => $value_valid_url,
     value_none     => ''
 };
 $var_list{'MM_MEDIA_VIDEO_MOUNTPOINT'} =
 {
-    value_default  => '/mnt/video'
+    value_default  => '/mnt/video',
+    value_valid    => $value_valid_mountpoint
 };
 $var_list{'MM_MEDIA_VIDEO_URL'} =
 {
     value_default  => 'none',
-    value_valid    => 'none|((cifs|nfs):(//(([^:@]*)?(:([^@]*))?\@)?([^/]+))?[^?#]*(\?([^#]*))?(\#(.*))?)',
+    value_valid    => $value_valid_url,
     value_none     => ''
 };
 $var_list{'MM_MEDIA_DVD_RIP_MOUNTPOINT'} =
 {
-    value_default  => '/mnt/dvd'
+    value_default  => '/mnt/dvd',
+    value_valid    => $value_valid_mountpoint
 };
 $var_list{'MM_MEDIA_DVD_RIP_URL'} =
 {
     value_default  => 'none',
-    value_valid    => 'none|((cifs|nfs):(//(([^:@]*)?(:([^@]*))?\@)?([^/]+))?[^?#]*(\?([^#]*))?(\#(.*))?)',
+    value_valid    => $value_valid_url,
+    value_none     => ''
+};
+$var_list{'MM_MEDIA_GENERIC_LIST'} =
+{
+    value_clean    => $value_clean_list,
+    value_default  => 'none',
+    value_valid    => $value_valid_list,
     value_none     => ''
 };
 
