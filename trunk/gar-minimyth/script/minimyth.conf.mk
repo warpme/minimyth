78d75471510414b8b6e2b250357a3d0a95c31ec7
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-11-14 15:05:32 +0000

    - Bumped MiniMyth version. - Bumped MiniMyth conf version.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -12,13 +12,13 @@ mm_VERSION_MYTH           ?= $(strip \
                                 $(if $(filter 0.21 ,        $(mm_MYTH_VERSION)),0.21.0                        ) \
                                 $(if $(filter trunk,        $(mm_MYTH_VERSION)),trunk.$(mm_MYTH_TRUNK_VERSION)) \
                               )
-mm_VERSION_MINIMYTH       ?= 59
+mm_VERSION_MINIMYTH       ?= 60
 mm_VERSION_EXTRA          ?= $(strip \
                                 $(if $(filter yes,$(mm_DEBUG)),-debug) \
                               )
 
 # Configuration file (minimyth.conf) version.
-mm_CONF_VERSION           ?= 36
+mm_CONF_VERSION           ?= 37
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
