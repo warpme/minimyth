6307b3ae9463d56c3d1a794f9b5ddf32102a419b
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-11-19 17:54:15 +0000

    - Disabled use of out-of-tree ALSA 1.0.18a drivers as some of the drivers   fail after suspend-to-RAM. - Reverted to kernel 2.6.26 bacause wake-on-USB doe now work under kernel   2.6.27. - Added patch that may help pervent NFS failures after suspend-to-RAM.

--- trunk/gar-minimyth/script/meta/minimyth/files/check.mk
+++ trunk/gar-minimyth/script/meta/minimyth/files/check.mk
@@ -168,12 +168,14 @@ mm-all:
 		fi ; \
 	done
 	@echo "    mm_KERNEL_HEADERS_VERSION"
-	@if [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "2.6.27" ] ; then \
+	@if [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "2.6.26" ] && \
+	    [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "2.6.27" ] ; then \
 		echo "error: mm_KERNEL_HEADERS_VERSION=\"$(mm_KERNEL_HEADERS_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
 	@echo "    mm_KERNEL_VERSION"
-	@if [ ! "$(mm_KERNEL_VERSION)" = "2.6.27" ] ; then \
+	@if [ ! "$(mm_KERNEL_VERSION)" = "2.6.26" ] && \
+	    [ ! "$(mm_KERNEL_VERSION)" = "2.6.27" ] ; then \
 		echo "error: mm_KERNEL_VERSION=\"$(mm_KERNEL_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
