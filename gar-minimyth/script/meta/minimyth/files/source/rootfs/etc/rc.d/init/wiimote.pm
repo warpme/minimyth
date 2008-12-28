2c33888236ffa78aeb78460da9c400aa8c9b7f8b
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2008-12-28 17:44:40 +0000

    - Improved checking for whether or not a Wiimote is in use before   initializing during init.

--- gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/wiimote.pm
+++ gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/wiimote.pm
@@ -60,12 +60,6 @@ sub start
 
         $minimyth->message_output('info', "starting wminput ...");
 
-        # wminput is already running, so return.
-        if ($minimyth->application_running('wminput'))
-        {
-            return 1;
-        }
-
         # Load uinput driver as needed.
         if (! -e q(/dev/uinput))
         {
@@ -96,7 +90,28 @@ sub start
                 $minimyth->message_output('err', "error: no 'wminput' config file found.");
                 return 0;
             }
-            system(qq(/usr/bin/wminput -d -c $config $address_0 > $devnull 2>&1 &));
+            my $running = 0;
+            if (open(FILE, '-|', q(/usr/bin/hcitool con 2> /dev/null)))
+            {
+                while (<FILE>)
+                {
+                    chomp;
+                    if (/^[[:cntrl:]]*< ACL $address_0/)
+                    {
+                        $running = 1;
+                        last;
+                    }
+                }
+                close(FILE);
+            }
+            if (! $running)
+            {
+                system(qq(/usr/bin/wminput -d -c $config $address_0 > $devnull 2>&1 &));
+            }
+            else
+            {
+                $minimyth->message_log('info', qq(Wiimote 0 \(address: $address_0\) not started because it is already running));
+            }
         }
         if ($address_1)
         {
@@ -114,7 +129,28 @@ sub start
                 $minimyth->message_output('err', "error: no 'wminput' config file found.");
                 return 0;
             }
-            system(qq(/usr/bin/wminput -d -c $config $address_1 > $devnull 2>&1 &));
+            my $running = 0;
+            if (open(FILE, '-|', q(/usr/bin/hcitool con 2> /dev/null)))
+            {
+                while (<FILE>)
+                {
+                    chomp;
+                    if (/^[[:cntrl:]]*< ACL $address_1/)
+                    {
+                        $running = 1;
+                        last;
+                    }
+                }
+                close(FILE);
+            }
+            if (! $running)
+            {
+                system(qq(/usr/bin/wminput -d -c $config $address_1 > $devnull 2>&1 &));
+            }
+            else
+            {
+                $minimyth->message_log('info', qq(Wiimote 1 \(address: $address_1\) not started because it is already running));
+            }
         }
         if ($address_2)
         {
@@ -132,7 +168,28 @@ sub start
                 $minimyth->message_output('err', "error: no 'wminput' config file found.");
                 return 0;
             }
-            system(qq(/usr/bin/wminput -d -c $config $address_2 > $devnull 2>&1 &));
+            my $running = 0;
+            if (open(FILE, '-|', q(/usr/bin/hcitool con 2> /dev/null)))
+            {
+                while (<FILE>)
+                {
+                    chomp;
+                    if (/^[[:cntrl:]]*< ACL $address_2/)
+                    {
+                        $running = 1;
+                        last;
+                    }
+                }
+                close(FILE);
+            }
+            if (! $running)
+            {
+                system(qq(/usr/bin/wminput -d -c $config $address_2 > $devnull 2>&1 &));
+            }
+            else
+            {
+                $minimyth->message_log('info', qq(Wiimote 2 \(address: $address_2\) not started because it is already running));
+            }
         }
         if ($address_3)
         {
@@ -150,7 +207,28 @@ sub start
                 $minimyth->message_output('err', "error: no 'wminput' config file found.");
                 return 0;
             }
-            system(qq(/usr/bin/wminput -d -c $config $address_3 > $devnull 2>&1 &));
+            my $running = 0;
+            if (open(FILE, '-|', q(/usr/bin/hcitool con 2> /dev/null)))
+            {
+                while (<FILE>)
+                {
+                    chomp;
+                    if (/^[[:cntrl:]]*< ACL $address_3/)
+                    {
+                        $running = 1;
+                        last;
+                    }
+                }
+                close(FILE);
+            }
+            if (! $running)
+            {
+                system(qq(/usr/bin/wminput -d -c $config $address_3 > $devnull 2>&1 &));
+            }
+            else
+            {
+                $minimyth->message_log('info', qq(Wiimote 3 \(address: $address_3\) not started because it is already running));
+            }
         }
     }
 
