491b146d3b1c77bcba886d4d79fa20c03094e89f
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2009-03-31 02:58:00 +0000

    - Removed packages nvidia/nvidia-180.29 and nvidia/nvidia-180.41. - Added package nvidia/nvidia-180.44.

--- trunk/gar-minimyth/script/meta/minimyth/files/check.mk
+++ trunk/gar-minimyth/script/meta/minimyth/files/check.mk
@@ -209,8 +209,7 @@ mm-all:
 	    [ ! "$(mm_NVIDIA_VERSION)" = "96.43.11"  ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "169.12"    ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "173.14.18" ] && \
-	    [ ! "$(mm_NVIDIA_VERSION)" = "180.29"    ] && \
-	    [ ! "$(mm_NVIDIA_VERSION)" = "180.41"    ] ; then \
+	    [ ! "$(mm_NVIDIA_VERSION)" = "180.44"    ] ; then \
 		echo "error: mm_NVIDIA_VERSION=\"$(mm_NVIDIA_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
