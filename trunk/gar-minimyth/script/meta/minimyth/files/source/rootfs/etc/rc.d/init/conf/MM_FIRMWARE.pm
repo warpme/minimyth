d7850594681999298a2dca2b1c6c99597e23c7f7
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2009-05-06 03:33:07 +0000

    - Improved init     - Changed init so that firmware files are fetched before firmware       dependent kernel drivers are loaded. - Modified MiniMyth configuration     - Added MM_FIRMWARE_FILE_LIST.     - Obsoleted MM_TUNER_FIRMWARE_FILE_LIST as it has been replaced by the more       general MM_FIRMWARE_FILE_LIST.

---
+++ trunk/gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/conf/MM_FIRMWARE.pm
@@ -0,0 +1,66 @@
+################################################################################
+# MM_FIRMWARE configuration variable handlers.
+################################################################################
+package init::conf::MM_FIRMWARE;
+
+use strict;
+use warnings;
+
+use File::Basename ();
+
+my %var_list;
+
+sub var_list
+{
+    return \%var_list;
+}
+
+$var_list{'MM_FIRMWARE_FILE_LIST'} =
+{
+    value_default => 'auto',
+    value_valid   => 'auto|none|.*',
+    value_auto    => sub
+    {
+        my $minimyth = shift;
+        my $name     = shift;
+
+        my @value_auto = ();
+
+        my @firmwares = @{$minimyth->detect_state_get('firmware')};
+        foreach my $firmware (@firmwares)
+        {
+            push(@value_auto, split(/:/, $firmware->{file_list}));
+        }
+
+        # Remove any duplicates.
+        {
+            my $prev = '';
+            @value_auto = grep($_ ne $prev && (($prev) = $_), sort(@value_auto));
+        }
+
+        return join(' ', @value_auto);
+    },
+    value_none    => '',
+    value_file    => '.+',
+    file          => sub
+    {
+        my $minimyth = shift;
+        my $name     = shift;
+
+        my @file = ();
+
+        foreach (split(/ /, $minimyth->var_get($name)))
+        {
+            my $item;
+            $item->{'name_remote'} = "$_";
+            $item->{'name_local'}  = '/lib/firmware/' . File::Basename::basename($_);
+            $item->{'mode_local'}  = '0644';
+
+            push(@file, $item);
+        }
+
+        return \@file;
+    }
+};
+
+1;
