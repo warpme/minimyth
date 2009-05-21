f39c136f0624c4a2a400a87f8ca74dd0ae2d1a45
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2009-05-21 01:49:51 +0000

    - Added the LCDproc kernel module to the manual kernel module list.       - A modprobe.conf file is created for the LCDproc kernel module         options.       - The LCDproc device is checked for by MM_LCDPROC_DEVICE. - Added LIRC device checking by MM_LIRC_DEVICE.

--- gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/lcdproc.pm
+++ gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/lcdproc.pm
@@ -19,27 +19,8 @@ sub start
     # Only start the LCDd daemon when one is not running already.
     if (! $minimyth->application_running('LCDd'))
     {
-        my $device                = $minimyth->var_get('MM_LCDPROC_DEVICE');
-        my $driver                = $minimyth->var_get('MM_LCDPROC_DRIVER');
-        my $kernel_module         = $minimyth->var_get('MM_LCDPROC_KERNEL_MODULE');
-        my $kernel_module_options = $minimyth->var_get('MM_LCDPROC_KERNEL_MODULE_OPTIONS');
-
-        # Load user configured kernel module.
-        if ($kernel_module)
-        {
-            system(qq(/sbin/modprobe $kernel_module $kernel_module_options));
-            # Wait up to 60 seconds for the device to appear in the device file system.
-            for (my $delay = 0 ; ($delay < 60) && (! -e $device) ; $delay++)
-            {
-                $minimyth->message_output('info', "waiting for LCD/VFD device ($delay seconds) ...");
-                sleep 1;
-            }
-            if (! -e $device)
-            {
-                $minimyth->message_output('err', "timed out waiting for LCD/VFD device.");
-                return 0;
-            }
-        }
+        my $driver = $minimyth->var_get('MM_LCDPROC_DRIVER');
+        my $device = $minimyth->var_get('MM_LCDPROC_DEVICE');
 
         # Load the LCDproc configuration file.
         if (! -e '/etc/LCDd.conf')
