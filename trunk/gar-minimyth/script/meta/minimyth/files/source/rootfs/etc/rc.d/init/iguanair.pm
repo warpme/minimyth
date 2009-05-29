89c5aaeb0987a7ce22a884f11f40c59dd1f16db2
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2009-05-29 15:23:09 +0000

    - Added iguanaIR init support for LIRC functions. - Added packages lib/popt and system/iguanair.

---
+++ trunk/gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/iguanair.pm
@@ -0,0 +1,46 @@
+################################################################################
+# iguanair
+################################################################################
+package init::iguanair;
+
+use strict;
+use warnings;
+
+use MiniMyth ();
+
+sub start
+{
+    my $self     = shift;
+    my $minimyth = shift;
+
+    my $igdaemon_enabled = 'no';
+    my @device_list = split(/ +/, $minimyth->var_get('MM_LIRC_DEVICE_LIST'));
+    foreach my $device_item (@device_list)
+    {
+        my (undef, $driver, undef) = split(/,/, $device_item);
+        if ($driver eq 'iguanaIR')
+        {
+            $igdaemon_enabled = 'yes';
+        }
+    }
+    if ($igdaemon_enabled eq 'yes')
+    {
+        $minimyth->message_output('info', "starting iguanaIR daemon ...");
+        system('/usr/bin/igdaemon',
+               '--log-file=/var/log/igdaemon.log',
+               '--pid-file=/var/run/igdaemon.pid')
+    }
+    return 1;
+}
+
+sub stop
+{
+    my $self     = shift;
+    my $minimyth = shift;
+
+    $minimyth->message_stop('igdeamon', "stopping iguanaIR daemon ...");
+
+    return 1;
+}
+
+1;
