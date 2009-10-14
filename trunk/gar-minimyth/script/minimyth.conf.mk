971458df32a6fde6e74d54a127cab102d47248e5
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2009-10-14 20:35:45 +0000

    - Added MM_HULU_STORE_HULUDESKTOP_DATA.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -19,7 +19,7 @@ mm_VERSION_EXTRA          ?= $(strip \
                               )
 
 # Configuration file (minimyth.conf) version.
-mm_CONF_VERSION           ?= 48
+mm_CONF_VERSION           ?= 49
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
