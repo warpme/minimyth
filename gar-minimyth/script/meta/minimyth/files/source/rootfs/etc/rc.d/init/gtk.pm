0aaf9baca3992acb058c384aeae2aca6d900c72f
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2009-11-07 12:34:15 +0000

    - Separate D-Bus init from GTK init.

--- gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/gtk.pm
+++ gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/gtk.pm
@@ -16,17 +16,6 @@ sub start
 
     $minimyth->message_output('info', "generating GTK related configuration files ...");
 
-    if (-e q(/usr/bin/dbus-uuidgen))
-    {
-        if (! -e q(/var/lib/dbus))
-        {
-            File::Path::mkpath(q(/var/lib/dbus), { mode => 0755 });
-        }
-        chmod(0755, q(/var/lib/dbus));
-        system(qq(/usr/bin/dbus-uuidgen > /var/lib/dbus/machine-id));
-        chmod(0644, q(/var/lib/dbus/machine-id));
-    }
-
     if (-e q(/usr/bin/gdk-pixbuf-query-loaders))
     {
         if (! -e q(/etc/gtk-2.0))
