5304324e4dc0af246d758b4537d54fab262437f5
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2009-11-11 05:37:09 +0000

    - Modified how MM_HULU_URL and MM_FLASH_URL are defaulted.

--- trunk/gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/conf/MM_FLASH.pm
+++ trunk/gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/conf/MM_FLASH.pm
@@ -15,7 +15,7 @@ sub var_list
 
 $var_list{'MM_FLASH_URL'} =
 {
-    prerequisite   => ['MM_PLUGIN_BROWSER_ENABLED'],
+    prerequisite   => ['MM_HULU_URL', 'MM_PLUGIN_BROWSER_ENABLED'],
     value_default  => 'auto',
     value_valid    => 'auto|none|((cifs|confro|confrw|dist|file|http|hunt|nfs|tftp):(//(([^:@]*)?(:([^@]*))?\@)?([^/]+))?[^?#]*(\?([^#]*))?(\#(.*))?)',
     value_auto     => sub
@@ -25,15 +25,13 @@ $var_list{'MM_FLASH_URL'} =
 
         my $value_auto = 'none';
 
-        if ($minimyth->var_get('MM_PLUGIN_BROWSER_ENABLED') eq 'yes')
+        if (($minimyth->var_get('MM_HULU_URL') ne '')                  ||
+            ($minimyth->var_get('MM_PLUGIN_BROWSER_ENABLED') eq 'yes'))
         {
             if (-e '/lib/ld-linux.so.2')
             {
                 $value_auto = 'confrw:libflashplayer.32.so';
             }
-        }
-        if ($minimyth->var_get('MM_PLUGIN_BROWSER_ENABLED') eq 'yes')
-        {
             if (-e '/lib/ld-linux-x86-64.so.2')
             {
                 $value_auto = 'confrw:libflashplayer.64.so';
