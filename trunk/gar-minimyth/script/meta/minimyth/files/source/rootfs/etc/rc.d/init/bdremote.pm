8f441715bcdc4949e3a060f9b786317bc8cbcfa5
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2009-12-16 23:57:26 +0000

    - Obsoleted MM_LIRC_DEVICE_LIST as the change to udev hotplug makes it obsolete.

--- trunk/gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/bdremote.pm
+++ trunk/gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/bdremote.pm
@@ -13,26 +13,14 @@ sub start
     my $self     = shift;
     my $minimyth = shift;
 
-    my $device = undef;
-    my $driver = undef;
-
-    my @device_list = split(/ +/, $minimyth->var_get('MM_LIRC_DEVICE_LIST'));
-    foreach my $device_item (@device_list)
-    {
-        ($device, $driver, undef) = split(/,/, $device_item);
-        if ($driver eq 'bdremote')
-        {
-            last;
-        }
-    }
-    if ($driver eq 'bdremote')
+    if ($minimyth->var_get('MM_LIRC_DRIVER') eq 'bdremote')
     {
         $minimyth->message_output('info', "starting Sony PS3 Blu-ray Disc Remote Control daemon ...");
 
         my $daemon = '/usr/sbin/bdremoteng';
         $daemon = $daemon . " -l";
         $daemon = $daemon . " -E";
-        $daemon = $daemon . " -a $device";
+        $daemon = $daemon . " -a " . $minimyth->var_get('MM_LIRC_DEVICE');
         $daemon = $daemon . " -t 30";
         $daemon = $daemon . " -p 8765";
         system(qq($daemon));
