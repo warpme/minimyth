c38aba37f20d617f659bf4f69cf1e07fc4461717
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2010-01-09 16:02:08 +0000

    - Updated MythTV 0.22 and trunk versions.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -136,7 +136,7 @@ mm_NVIDIA_VERSION         ?= 190.53
 mm_XORG_VERSION           ?= 7.5
 # Myth trunk version built. If the version changes too much then the patches may
 # no longer work.
-mm_MYTH_TRUNK_VERSION     ?= 23011
+mm_MYTH_TRUNK_VERSION     ?= 23095
 # Lists additional packages to build when minimyth is built.
 mm_USER_PACKAGES          ?=
 # Lists additional binaries to include in the MiniMyth image
