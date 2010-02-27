69a03ed5a73a6bde13a5b55966e957a9267d0f3b
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2010-02-27 15:48:29 +0000

    - Added package nvidia/nvidia-195.36.08.

--- gar-minimyth/script/meta/minimyth/files/check.mk
+++ gar-minimyth/script/meta/minimyth/files/check.mk
@@ -209,7 +209,8 @@ mm-all:
 	@if [ ! "$(mm_NVIDIA_VERSION)" = "96.43.16"  ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "173.14.25" ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "185.18.36" ] && \
-	    [ ! "$(mm_NVIDIA_VERSION)" = "190.53"    ] ; then \
+	    [ ! "$(mm_NVIDIA_VERSION)" = "190.53"    ] && \
+	    [ ! "$(mm_NVIDIA_VERSION)" = "195.36.08" ] ; then \
 		echo "error: mm_NVIDIA_VERSION=\"$(mm_NVIDIA_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
