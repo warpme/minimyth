9fe2dc48cb1bf1322d82113199262b6cb0ce839c
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2010-06-30 19:48:34 +0000

    - Fixed python/python dependecies. - Fixed bug in cross patch openssl version.

--- gar-minimyth/script/python/python/package-api.mk
+++ gar-minimyth/script/python/python/package-api.mk
@@ -1,6 +1,9 @@
-PYTHON_VERSION = 2.6.5
+PYTHON_VERSION_MAJOR = 2
+PYTHON_VERSION_MINOR = 6
+PYTHON_VERSION_TEENY = 5
+PYTHON_VERSION = $(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR).$(PYTHON_VERSION_TEENY)
 
-PYTHON_includedir = $(includedir)/python2.6
-PYTHON_libdir = $(libdir)/python2.6
-build_PYTHON_includedir = $(build_includedir)/python2.6
-build_PYTHON_libdir = $(build_libdir)/python2.6
+PYTHON_includedir = $(includedir)/python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)
+PYTHON_libdir = $(libdir)/python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)
+build_PYTHON_includedir = $(build_includedir)/python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)
+build_PYTHON_libdir = $(build_libdir)/python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)
