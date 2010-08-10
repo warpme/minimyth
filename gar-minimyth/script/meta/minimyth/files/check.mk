f6d913914549fc508df4f199cedb76b852d7a22d
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2010-08-10 22:58:44 +0000

    - Added packages kernel-2.6.35/* kernel-headers/linux-headers-2.6.35.

--- gar-minimyth/script/meta/minimyth/files/check.mk
+++ gar-minimyth/script/meta/minimyth/files/check.mk
@@ -180,12 +180,14 @@ mm-all:
 		fi ; \
 	done
 	@echo "    mm_KERNEL_HEADERS_VERSION"
-	@if [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "2.6.34" ] ; then \
+	@if [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "2.6.34" ] && \
+	    [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "2.6.35" ] ; then \
 		echo "error: mm_KERNEL_HEADERS_VERSION=\"$(mm_KERNEL_HEADERS_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
 	@echo "    mm_KERNEL_VERSION"
-	@if [ ! "$(mm_KERNEL_VERSION)" = "2.6.34" ] ; then \
+	@if [ ! "$(mm_KERNEL_VERSION)" = "2.6.34" ] && \
+	    [ ! "$(mm_KERNEL_VERSION)" = "2.6.35" ] ; then \
 		echo "error: mm_KERNEL_VERSION=\"$(mm_KERNEL_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -115,10 +115,10 @@ mm_TFTP_ROOT              ?= /var/tftpboot/minimyth
 # installed in a subdirectory named 'minimyth-$(mm_VERSION)'.
 mm_NFS_ROOT               ?= /home/public/minimyth
 # The version of kernel headers to use.
-# Valid values are '2.6.34'.
+# Valid values are '2.6.34' and '2.6.35' (does not work).
 mm_KERNEL_HEADERS_VERSION ?= 2.6.34
 # The version of kernel to use.
-# Valid values are '2.6.34'.
+# Valid values are '2.6.34' and '2.6.35' (does not work).
 mm_KERNEL_VERSION         ?= 2.6.34
 # The kernel configuration file to use.
 # When set, the kernel configuration file $(HOME)/.minimyth/$(mm_KERNEL_CONFIG) will be used.
