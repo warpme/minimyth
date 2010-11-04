0ad11099a51534dbfe6cd97f398c672e090373bf
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2010-11-04 23:11:33 +0000

    - Removed package nvidia/nvidia-96.43.18.

--- trunk/gar-minimyth/script/meta/minimyth/files/check.mk
+++ trunk/gar-minimyth/script/meta/minimyth/files/check.mk
@@ -191,7 +191,6 @@ mm-all:
 	@echo "    mm_NVIDIA_VERSION"
 	@if [ ! "$(mm_NVIDIA_VERSION)" = "96.43.19"  ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "173.14.28" ] && \
-	    [ ! "$(mm_NVIDIA_VERSION)" = "256.53"    ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "260.19.12" ] ; then \
 		echo "error: mm_NVIDIA_VERSION=\"$(mm_NVIDIA_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
