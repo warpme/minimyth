f94e090747ace00c932c9851790b36fba07c42d3
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2010-11-09 16:52:13 +0000

    - Re-added package nvidia/nvidia-256.53 and made it the default to VDPAU problems with nvidia/nvidia-260.19.12 on some hardware.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -124,8 +124,8 @@ mm_KERNEL_CONFIG          ?=
 # Valid values are '0.22', '0.23', '0.24' and 'trunk'.
 mm_MYTH_VERSION           ?= 0.23
 # The version of the NVIDIA driver.
-# Valid values are '96.43.19' (legacy), '173.14.28' (legacy), '260.19.12'.
-mm_NVIDIA_VERSION         ?= 260.19.12
+# Valid values are '96.43.19' (legacy), '173.14.28' (legacy), '256.53', '260.19.12'.
+mm_NVIDIA_VERSION         ?= 256.53
 # The version of xorg to use.
 # Valid values are '7.4' and '7.5'.
 mm_XORG_VERSION           ?= 7.4
