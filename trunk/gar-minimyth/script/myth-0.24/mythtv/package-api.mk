92be6f4288145b8ac337dfebfc210b6a33b33c1d
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2010-12-02 22:34:14 +0000

    - Modified MythTV packages in preparation for MythTV's transition from SVN   to GIT (specifically github). - Updated MythTV 0.22, 0.23, 0.24 and trunk versions.

---
+++ trunk/gar-minimyth/script/myth-0.24/mythtv/package-api.mk
@@ -0,0 +1,25 @@
+MYTHTV_GARVERSION_SHORT = 0.24
+MYTHTV_SVN_VERSION      = 27419
+
+MYTHTV_VERSION = $(MYTHTV_GARVERSION_SHORT)-$(MYTHTV_SVN_VERSION)
+
+MYTHTV_SOURCEDIR = $(sourcedir)/mythtv
+
+MYTHTV_CONFIGURE_ENV = \
+	PYTHONXCPREFIX=$(DESTDIR)$(prefix)
+MYTHTV_BUILD_ENV     =
+MYTHTV_INSTALL_ENV   = \
+	INSTALL_ROOT=$(DESTDIR)
+
+MYTHTV_PLUGINS_CONFIGURE_ARGS = \
+	--prefix="$(prefix)" \
+	--sysroot="$(DESTDIR)$(rootdir)" \
+	--qmake="$(DESTDIR)$(qt4bindir)/qmake" \
+	--libdir-name="lib" \
+	--disable-all \
+	--enable-opengl
+
+post-install-mythtv-version:
+	@install -d $(DESTDIR)$(versiondir) 
+	@echo "$(MYTHTV_GARVERSION_SHORT) through SVN $(MYTHTV_SVN_VERSION)" > $(DESTDIR)$(versiondir)/$(GARNAME)
+	@$(MAKECOOKIE)
