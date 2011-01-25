f3af45d1efde4b09928c3f9386a7cc558233db88
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2011-01-25 18:52:02 +0000

    - Updated MythTV 0.24 and trunk versions.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -130,7 +130,7 @@ mm_XORG_VERSION           ?= 7.6
 # may no longer work. The version string format is:
 # master-<date>-<mythtv-git-commit>-<myththemes-git-commit>, where <date> has
 # the format YYYYMMDD.
-mm_MYTHTV_MASTER_VERSION  ?= master-20110114-5707d26-45d6b63
+mm_MYTHTV_MASTER_VERSION  ?= master-20110124-b4f1039-45d6b63
 # Lists additional packages to build when minimyth is built.
 mm_USER_PACKAGES          ?=
 # Lists additional binaries to include in the MiniMyth image
