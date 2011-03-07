3ec750b5ee6704a7e37ab2f74ab4867a39785529
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2011-03-07 22:53:56 +0000

    - Removed package nvidia/nvidia-260.19.36.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -122,7 +122,7 @@ mm_KERNEL_CONFIG          ?=
 # Valid values are '0.22', '0.23', '0.24' and 'master'.
 mm_MYTH_VERSION           ?= 0.24
 # The version of the NVIDIA driver.
-# Valid values are '96.43.19' (legacy), '173.14.28' (legacy), '260.19.36', '260.19.44'.
+# Valid values are '96.43.19' (legacy), '173.14.28' (legacy), '260.19.44'.
 mm_NVIDIA_VERSION         ?= 260.19.44
 # The version of xorg to use.
 # Valid values are '7.4' and '7.6'.
