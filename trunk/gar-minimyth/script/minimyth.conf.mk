676f5f66381d698adfb1d45c6428b2fd4179c443
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2011-03-09 03:59:21 +0000

    - Switched to the default kernel to 2.6.37 now that the problem with my bluetooth adapter has been fixed.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -110,10 +110,10 @@ mm_TFTP_ROOT              ?= /var/tftpboot/minimyth
 mm_NFS_ROOT               ?= /home/public/minimyth
 # The version of kernel headers to use.
 # Valid values are '2.6.36' and '2.6.37'.
-mm_KERNEL_HEADERS_VERSION ?= 2.6.36
+mm_KERNEL_HEADERS_VERSION ?= 2.6.37
 # The version of kernel to use.
 # Valid values are ''2.6.36' and 2.6.37'.
-mm_KERNEL_VERSION         ?= 2.6.36
+mm_KERNEL_VERSION         ?= 2.6.37
 # The kernel configuration file to use.
 # When set, the kernel configuration file $(HOME)/.minimyth/$(mm_KERNEL_CONFIG) will be used.
 # When not set, a built-in kernel configuration file will be used.
