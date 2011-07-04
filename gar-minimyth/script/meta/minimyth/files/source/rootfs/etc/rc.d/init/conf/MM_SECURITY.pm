82989cbbdbeac4941f590c89690df35b11c04ac0
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2011-07-04 22:16:53 +0000

    - Added package data/ca-bundle.crt. - Relocated ca-bundle.crt from /etc/ssl/certs to /etc/pki/tls/certs. - Modified MM_SECURITY_FETCH_CA_BUNDLE_CRT so that it is prepended to   MiniMyth's built-in ca-bundle.crt. - Fixed a bug that caused https connections using MythBrowser to fail.

--- gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/conf/MM_SECURITY.pm
+++ gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/conf/MM_SECURITY.pm
@@ -34,7 +34,39 @@ $var_list{'MM_SECURITY_FETCH_CA_BUNDLE_CRT'} =
     value_valid    => 'no|yes',
     value_file     => 'yes',
     file           => {name_remote => '/ca-bundle.crt',
-                       name_local  => '/etc/ssl/certs/ca-bundle.crt'}
+                       name_local  => '/etc/pki/tls/certs/ca-bundle.crt'},
+    extra          => sub
+    {
+        my $minimyth = shift;
+        my $name     = shift;
+
+        # Append the build-in ca-bundle.crt to the just fetched user ca-bundle.crt.
+        if (! open(I_FILE, '<', '/initrd/rootfs-ro/etc/pki/tls/certs/ca-bundle.crt'))
+        {
+            $minimyth->message_output('err', qq(failed to open file '/initrd/rootfs-ro/etc/pki/tls/certs/ca-bundle.crt' for reading));
+            return 0;
+        }
+        if (! open(O_FILE, '>>', '/etc/pki/tls/certs/ca-bundle.crt'))
+        {
+            $minimyth->message_output('err', qq(failed to open file '/etc/pki/tls/certs/ca-bundle.crt' for appending));
+            close(I_FILE);
+            return 0;
+        }
+        print O_FILE "\n";
+        print O_FILE "# \n";
+        print O_FILE "# Begin built-in bundle of digital certificates for trusted certificate authorities.\n";
+        print O_FILE "# \n";
+        print O_FILE "\n";
+        while(<I_FILE>)
+        {
+            print O_FILE $_;
+        }
+        close(O_FILE);
+        close(I_FILE);
+
+        return 1;
+    }
+
 };
 $var_list{'MM_SECURITY_FETCH_CREDENTIALS_CIFS'} =
 {
