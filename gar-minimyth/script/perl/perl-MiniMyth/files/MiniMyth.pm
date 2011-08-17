b9adeb20baf10a2776fa64ee6f334e8dbcc3eb37
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2011-08-17 02:29:10 +0000

    - Updated the Adobe Flash Player fetch and save command to fetch and save   Adobe Flash Player 11.0 beta 2.

--- gar-minimyth/script/perl/perl-MiniMyth/files/MiniMyth.pm
+++ gar-minimyth/script/perl/perl-MiniMyth/files/MiniMyth.pm
@@ -1900,15 +1900,15 @@ sub flash_fetch_and_save
     {
         $flash_file = q(libflashplayer.32.so);
         ## 10.3 release.
-        #$flash_url  = q(http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player_10_linux.tar.gz);
-        # 11.0 beta 1.
-        $flash_url  = q(http://download.macromedia.com/pub/labs/flashplatformruntimes/flashplayer11/flashplayer11_b1_install_lin_32_071311.tar.gz);
+        #$flash_url = q(http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player_10_linux.tar.gz);
+        # 11.0 beta 2.
+        $flash_url = q(http://download.macromedia.com/pub/labs/flashplatformruntimes/flashplayer11/flashplayer11_b2_install_lin_32_080811.tar.gz);
     }
     elsif (-e q(/lib/ld-linux-x86-64.so.2))
     {
         $flash_file = q(libflashplayer.64.so);
-        # 11.0 beta 1.
-        $flash_url = q(http://download.macromedia.com/pub/labs/flashplatformruntimes/flashplayer11/flashplayer11_b1_install_lin_64_071311.tar.gz);
+        # 11.0 beta 2.
+        $flash_url = q(http://download.macromedia.com/pub/labs/flashplatformruntimes/flashplayer11/flashplayer11_b2_install_lin_64_080811.tar.gz);
     }
     else
     {
