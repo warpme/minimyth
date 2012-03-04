1190c3f35510683d0d2f51cff976c165780cfaad
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-03-04 04:54:19 +0000

    - Removed Linux 3.1 from the build check.

--- gar-minimyth/script/meta/minimyth/files/check.mk
+++ gar-minimyth/script/meta/minimyth/files/check.mk
@@ -174,14 +174,12 @@ mm-all:
 		fi ; \
 	done
 	@echo "    mm_KERNEL_HEADERS_VERSION"
-	@if [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "3.1" ] && \
-	    [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "3.2" ] ; then \
+	@if [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "3.2" ] ; then \
 		echo "error: mm_KERNEL_HEADERS_VERSION=\"$(mm_KERNEL_HEADERS_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
 	@echo "    mm_KERNEL_VERSION"
-	@if [ ! "$(mm_KERNEL_VERSION)" = "3.1" ] && \
-	    [ ! "$(mm_KERNEL_VERSION)" = "3.2" ] ; then \
+	@if [ ! "$(mm_KERNEL_VERSION)" = "3.2" ] ; then \
 		echo "error: mm_KERNEL_VERSION=\"$(mm_KERNEL_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
