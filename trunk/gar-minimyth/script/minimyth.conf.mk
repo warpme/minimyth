25482590dadc88b751e4074533a189a8cb7a964f
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-03-08 18:02:35 +0000

    - Changed the default NVIDIA driver.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -124,7 +124,7 @@ mm_MYTH_VERSION           ?= 0.24
 # The version of the NVIDIA driver.
 # Valid values are '96.43.20' (legacy), '173.14.31' (legacy), '270.41.19',
 # '275.36', '295.20'.
-mm_NVIDIA_VERSION         ?= 295.20
+mm_NVIDIA_VERSION         ?= 275.36
 # The version of xorg to use.
 # Valid values are '7.6'.
 mm_XORG_VERSION           ?= 7.6
