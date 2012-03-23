d4bd0ba34f5bfda045582984db640cfa78f8b290
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-03-23 01:49:27 +0000

    - Added package nvidia/nvidia-295.33.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -123,8 +123,8 @@ mm_KERNEL_CONFIG          ?=
 mm_MYTH_VERSION           ?= 0.24
 # The version of the NVIDIA driver.
 # Valid values are '96.43.20' (legacy), '173.14.31' (legacy), '270.41.19',
-# '275.36', '295.20'.
-mm_NVIDIA_VERSION         ?= 295.20
+# '275.36', '295.20', '295.33'.
+mm_NVIDIA_VERSION         ?= 295.33
 # The version of xorg to use.
 # Valid values are '7.6'.
 mm_XORG_VERSION           ?= 7.6
