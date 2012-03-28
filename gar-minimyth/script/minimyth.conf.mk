ccdaedc074e2fd12aa8761b314d91cdc376dedd4
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-03-28 03:55:07 +0000

    - Disabled gcc graphite support during compilation as it was causing some   hard to trace problems with some packages. - Cleaned up gcc link time optimizaton and whole program flags in multiple   packages. - Added gcc 4.7 patches missing from MythTV packages.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -175,11 +175,6 @@ mm_CFLAGS                 ?= $(strip \
                                  $(if $(filter c3-2        ,$(mm_GARCH)),-march=c3-2        -mtune=c3-2    -Os -mfpmath=sse) \
                                  $(if $(filter pentium-mmx ,$(mm_GARCH)),-march=pentium-mmx -mtune=generic -Os             ) \
                                  $(if $(filter x86-64      ,$(mm_GARCH)),-march=x86-64      -mtune=generic -O3 -mfpmath=sse) \
-                                 -ftree-loop-distribution                                                                    \
-                                 -floop-interchange                                                                          \
-                                 -floop-strip-mine                                                                           \
-                                 -floop-block                                                                                \
-                                 -fgraphite-identity                                                                         \
                                  -flto                                                                                       \
                                  $(if $(filter i386  ,$(mm_GARCH_FAMILY)),-m32)                                              \
                                  $(if $(filter x86_64,$(mm_GARCH_FAMILY)),-m64)                                              \
