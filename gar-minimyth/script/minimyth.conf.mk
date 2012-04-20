52539dbcebc89a2615a56bab7569835e2dba3a45
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-04-20 00:22:40 +0000

    - Added MM_AIRPLAY_FETCH_RAOPKEY_RSA for fetching the private key needed to   enable Apple AirPlay support in MythTV 0.25 and MythTV master.

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -19,7 +19,7 @@ mm_VERSION_EXTRA          ?= $(strip \
                               )
 
 # Configuration file (minimyth.conf) version.
-mm_CONF_VERSION           ?= 59
+mm_CONF_VERSION           ?= 80
 
 #-------------------------------------------------------------------------------
 # Variables that you are likely to be override based on your environment.
