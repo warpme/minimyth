a06cc4d336887fa23549671b0e715a1d9dba0c12
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-07-04 03:17:05 +0000

    - Removed packages kernel-3.3/* and  kernel-headers/linux-headers-3.3.

--- trunk/gar-minimyth/script/meta/minimyth/files/check.mk
+++ trunk/gar-minimyth/script/meta/minimyth/files/check.mk
@@ -176,14 +176,12 @@ mm-all:
 		fi ; \
 	done
 	@echo "    mm_KERNEL_HEADERS_VERSION"
-	@if [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "3.3" ] && \
-	    [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "3.4" ] ; then \
+	@if [ ! "$(mm_KERNEL_HEADERS_VERSION)" = "3.4" ] ; then \
 		echo "error: mm_KERNEL_HEADERS_VERSION=\"$(mm_KERNEL_HEADERS_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
 	@echo "    mm_KERNEL_VERSION"
-	@if [ ! "$(mm_KERNEL_VERSION)" = "3.3" ] && \
-	    [ ! "$(mm_KERNEL_VERSION)" = "3.4" ] ; then \
+	@if [ ! "$(mm_KERNEL_VERSION)" = "3.4" ] ; then \
 		echo "error: mm_KERNEL_VERSION=\"$(mm_KERNEL_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
