16c470dfcc93597e6aff021e0648bfe770aa8c85
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-07-04 03:09:59 +0000

    - Switched to using automatic microcode loading. - Added init code to split microcode.dat into intel-ucode/* so that is works with the microcode.ko module.

--- gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/cpu.pm
+++ gar-minimyth/script/meta/minimyth/files/source/rootfs/etc/rc.d/init/cpu.pm
@@ -16,41 +16,6 @@ sub start
 
     my $devnull = File::Spec->devnull;
 
-    if ($minimyth->var_get('MM_CPU_FETCH_MICROCODE_DAT') eq 'yes')
-    {
-        $minimyth->message_output('info', "loading CPU microcode ...");
-
-        if (! -e '/etc/firmware/microcode.dat')
-        {
-            $minimyth->message_output('err', "'/etc/firmware/microcode.dat' does not exist.");
-            return 0;
-        }
-
-        my $kernel_module = 'microcode';
-        if (system(qq(/sbin/modprobe $kernel_module > $devnull 2>&1)) != 0)
-        {
-            $minimyth->message_output('err', "failed to load kernel module: $kernel_module");
-            return 0;
-        }
-        system(qq(/usr/bin/udevadm settle --timeout=60));
-        if (! -e '/dev/cpu/microcode')
-        {
-            $minimyth->message_output('err', "'/dev/cpu/microcode' does not exist.");
-            return 0;
-        }
-        if (! -e '/usr/sbin/microcode_ctl')
-        {
-            $minimyth->message_output('err', "'/usr/sbin/microcode_ctl' does not exist.");
-            return 0;
-        }
-        if (system(qq(/usr/sbin/microcode_ctl -Q -f /etc/firmware/microcode.dat > $devnull 2>&1)) != 0)
-        {
-            $minimyth->message_output('err', "failed to load CPU microcode.");
-            return 0;
-        }
-        system(qq(/sbin/modprobe -r $kernel_module));
-    }
-
     $minimyth->message_output('info', "starting CPU frequency scaling ...");
 
     if ($minimyth->var_get('MM_CPU_FREQUENCY_GOVERNOR') ne 'performance')
