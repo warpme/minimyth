a48df3e4eb55d9d56ebab0c40e58e50b398ec0fd
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-07-06 04:49:02 +0000

    - Updated MythTV master (does not compile).

--- gar-minimyth/script/minimyth.conf.mk
+++ gar-minimyth/script/minimyth.conf.mk
@@ -134,7 +134,7 @@ mm_XORG_VERSION           ?= 7.6
 # may no longer work. The version string format is:
 # master-<date>-<mythtv-git-commit>-<myththemes-git-commit>, where <date> has
 # the format YYYYMMDD.
-mm_MYTHTV_MASTER_VERSION  ?= master-20120523-9b3d59c
+mm_MYTHTV_MASTER_VERSION  ?= master-20120702-e2a0efd
 # Lists additional packages to build when minimyth is built.
 mm_USER_PACKAGES          ?=
 # Lists additional binaries to include in the MiniMyth image
