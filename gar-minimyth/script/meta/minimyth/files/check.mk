b6eea1b565013c7946d89c35598e65ac960aff3f
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-10-03 23:27:50 +0000

    - Added package nvidia/nvidia-304.51. - Removed package nvidia/nvidia-304.43.

--- gar-minimyth/script/meta/minimyth/files/check.mk
+++ gar-minimyth/script/meta/minimyth/files/check.mk
@@ -203,7 +203,7 @@ mm-all:
 	@echo "    mm_NVIDIA_VERSION"
 	@if [ ! "$(mm_NVIDIA_VERSION)" = "295.59" ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "302.17" ] && \
-	    [ ! "$(mm_NVIDIA_VERSION)" = "304.43" ] ; then \
+	    [ ! "$(mm_NVIDIA_VERSION)" = "304.51" ] ; then \
 		echo "error: mm_NVIDIA_VERSION=\"$(mm_NVIDIA_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
