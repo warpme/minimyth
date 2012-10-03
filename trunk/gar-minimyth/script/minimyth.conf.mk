8d594d097eba5d1c324597b2574b6cb1737c1bbd
pebender <pebender@599ac98c-e21f-0410-93e3-3d01b592d70b>
2012-10-03 23:27:50 +0000

    - Added package nvidia/nvidia-304.51. - Removed package nvidia/nvidia-304.43.

--- trunk/gar-minimyth/script/minimyth.conf.mk
+++ trunk/gar-minimyth/script/minimyth.conf.mk
@@ -125,8 +125,8 @@ mm_KERNEL_CONFIG          ?=
 # Valid values are '0.22', '0.23', '0.24', '0.25', 'master'.
 mm_MYTH_VERSION           ?= 0.24
 # The version of the NVIDIA driver.
-# Valid values are '295.59', '302.17', '304.43'.
-mm_NVIDIA_VERSION         ?= 304.43
+# Valid values are '295.59', '302.17', '304.51'.
+mm_NVIDIA_VERSION         ?= 304.51
 # The version of xorg to use.
 # Valid values are '7.6'.
 mm_XORG_VERSION           ?= 7.6
