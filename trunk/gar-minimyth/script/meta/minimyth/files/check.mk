416167b8a7b25e7f148fefee9a3892f416a36d7e
pebender@gmail.com <pebender@gmail.com@599ac98c-e21f-0410-93e3-3d01b592d70b>
2015-06-12 06:32:48 +0000

    - Added packages myth-0.27/*. - Updated packages myth-0.25/*, myth-0.26 and myth-master/*.

--- trunk/gar-minimyth/script/meta/minimyth/files/check.mk
+++ trunk/gar-minimyth/script/meta/minimyth/files/check.mk
@@ -200,6 +200,7 @@ mm-all:
 	    [ ! "$(mm_MYTH_VERSION)" = "0.24"   ] && \
 	    [ ! "$(mm_MYTH_VERSION)" = "0.25"   ] && \
 	    [ ! "$(mm_MYTH_VERSION)" = "0.26"   ] && \
+	    [ ! "$(mm_MYTH_VERSION)" = "0.27"   ] && \
 	    [ ! "$(mm_MYTH_VERSION)" = "master" ] ; then \
 		echo "error: mm_MYTH_VERSION=\"$(mm_MYTH_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
---
+++ trunk/gar-minimyth/script/myth-0.27/mythtv/package-api.mk
@@ -0,0 +1,30 @@
+MYTHTV_GARVERSION = 0.27-20150529-b7afc9b
+
+MYTHTV_SOURCEDIR = $(sourcedir)/mythtv
+
+MYTHTV_CONFIGURE_ENV = \
+	PYTHONXCPREFIX=$(DESTDIR)$(prefix)
+MYTHTV_BUILD_ENV     =
+MYTHTV_INSTALL_ENV   = \
+	INSTALL_ROOT="/" \
+
+MYTHTV_PLUGINS_CONFIGURE_ARGS = \
+	--prefix="$(DESTDIR)$(prefix)" \
+	--sysroot="$(DESTDIR)$(rootdir)" \
+	--qmake="$(DESTDIR)$(qt4bindir)/qmake" \
+	--libdir-name="$(patsubst $(prefix)/%,%,$(libdir))" \
+	--disable-all \
+	--enable-opengl
+
+post-install-mythtv-version:
+	@install -d $(DESTDIR)$(versiondir) 
+	@rm -rf     $(DESTDIR)$(versiondir)/$(GARNAME)
+	@echo -n "$(MYTHTV_GARVERSION) "                                                  >> $(DESTDIR)$(versiondir)/$(GARNAME)
+	@echo -n "("                                                                      >> $(DESTDIR)$(versiondir)/$(GARNAME)
+	@echo -n "$(word 1,$(subst -, ,$(MYTHTV_GARVERSION)))"                            >> $(DESTDIR)$(versiondir)/$(GARNAME)
+	@echo -n " through "                                                              >> $(DESTDIR)$(versiondir)/$(GARNAME)
+	@echo -n "mythtv git repo commit $(word 3,$(subst -, ,$(MYTHTV_GARVERSION)))"     >> $(DESTDIR)$(versiondir)/$(GARNAME)
+	@echo -n " and "                                                                  >> $(DESTDIR)$(versiondir)/$(GARNAME)
+	@echo -n "myththemes git repo commit $(word 4,$(subst -, ,$(MYTHTV_GARVERSION)))" >> $(DESTDIR)$(versiondir)/$(GARNAME)
+	@echo    ")"                                                                      >> $(DESTDIR)$(versiondir)/$(GARNAME)
+	@$(MAKECOOKIE)
